8cdc976d5901839754fa6ae289bdb8bb3c0eb83d
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-02-09 15:30:25 +0900

    Commit miss...

---
+++ src/libaten/renderer/raytracer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "scene/scene.h"
+#include "camera/camera.h"
+
+namespace aten
+{
+	struct Destination {
+		int width{ 0 };
+		int height{ 0 };
+		uint32_t sample{ 1 };
+		uint32_t mutation{ 1 };
+		vec3* buffer{ nullptr };
+	};
+
+	class RayTracer {
+	public:
+		RayTracer() {}
+		~RayTracer() {}
+
+		void render(
+			Destination& dst,
+			scene* scene,
+			camera* camera);
+	};
+}

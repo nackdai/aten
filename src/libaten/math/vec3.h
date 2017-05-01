f3b540d401643af26eb13ab0aac0461ff5f381c4
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-05-01 14:43:57 +0900

    Fix a bug for microfacet material rendering.

--- src/libaten/math/vec3.h
+++ src/libaten/math/vec3.h
@@ -205,6 +205,7 @@ namespace aten {
 		// dot‚ğŒvZ‚µ‚½‚Æ‚«‚Éƒ[ƒ‚É‚È‚é‚æ‚¤‚ÈƒxƒNƒgƒ‹.
 		// k ‚Í normalize ŒvZ—p.
 
+#if 0
 		if (aten::abs(n.z) > real(0.707106781186547524401)) {
 			real k = aten::sqrt(n.y * n.y + n.z * n.z);
 			p.x = 0; 
@@ -217,6 +218,20 @@ namespace aten {
 			p.y = n.x / k; 
 			p.z = 0;
 		}
+#else
+		if (aten::abs(n.z) > real(0)) {
+			real k = aten::sqrt(n.y * n.y + n.z * n.z);
+			p.x = 0;
+			p.y = -n.z / k;
+			p.z = n.y / k;
+		}
+		else {
+			real k = aten::sqrt(n.x * n.x + n.y * n.y);
+			p.x = n.y / k;
+			p.y = -n.x / k;
+			p.z = 0;
+		}
+#endif
 
 		return std::move(p);
 	}

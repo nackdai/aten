ba70618e18ec0ee3c1cafc8f841cda4eb3616d74
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-05-26 15:48:31 +0900

    Separate shadow ray hit test, to avoid stack overflow.

--- src/libidaten/kernel/raytracing.cu
+++ src/libidaten/kernel/raytracing.cu
@@ -15,6 +15,15 @@
 
 #include "aten4idaten.h"
 
+struct ShadowRay : public aten::ray {
+	aten::vec3 lightPos;
+	int targetLightId;
+
+	struct {
+		uint32_t isActive : 1;
+	};
+};
+
 struct Path {
 	aten::ray ray;
 	aten::vec3 throughput;
@@ -99,9 +108,9 @@ __global__ void hitTestRayTracing(
 }
 
 __global__ void raytracing(
-	//float4* p,
 	cudaSurfaceObject_t outSurface,
 	Path* paths,
+	ShadowRay* shadowRays,
 	int width, int height,
 	aten::ShapeParameter* shapes, int geomnum,
 	aten::MaterialParameter* mtrls,
@@ -134,6 +143,8 @@ __global__ void raytracing(
 	const auto idx = iy * width + ix;
 
 	auto& path = paths[idx];
+
+	shadowRays[idx].isActive = false;
 
 	if (!path.isHit) {
 		return;
@@ -181,9 +192,12 @@ __global__ void raytracing(
 	}
 	else {
 		// TODO
-		auto light = lights[0];
-		auto* sphere = &ctxt.shapes[light.object.idx];
-		light.object.ptr = sphere;
+		int lightidx = 0;
+
+		auto light = lights[lightidx];
+		if (light.object.idx >= 0) {
+			light.object.ptr = &ctxt.shapes[light.object.idx];
+		}
 
 		aten::LightSampleResult sampleres;
 		sampleLight(&sampleres, &ctxt, &light, path.rec.p, nullptr);
@@ -193,43 +207,105 @@ __global__ void raytracing(
 
 		dirToLight.normalize();
 
-		aten::ray shadowRay(path.rec.p, dirToLight);
+		shadowRays[idx].isActive = true;
+		shadowRays[idx].org = path.rec.p;
+		shadowRays[idx].dir = dirToLight;
+		shadowRays[idx].lightPos = sampleres.pos;
+		shadowRays[idx].targetLightId = lightidx;
 
 		aten::hitrecord tmpRec;
 
-		bool isHit = intersectBVH(&ctxt, shadowRay, AT_MATH_EPSILON, AT_MATH_INF, &tmpRec);
-
-		if (AT_NAME::scene::hitLight(
-			isHit, 
-			&light, 
-			sampleres.pos, 
-			shadowRay, 
-			AT_MATH_EPSILON, AT_MATH_INF,
-			tmpRec.p,
-			tmpRec.t,
-			tmpRec.obj))
-		{
-			if (light.attrib.isInfinite) {
-				len = 1.0f;
-			}
-
-			const auto c0 = max(0.0f, dot(orienting_normal, dirToLight));
-			float c1 = 1.0f;
-
-			if (!light.attrib.isSingular) {
-				c1 = max(0.0f, dot(sampleres.nml, -dirToLight));
-			}
-
-			auto G = c0 * c1 / (len * len);
-
-			contrib += path.throughput * (mtrl->baseColor * sampleres.finalColor) * G;
+		if (light.attrib.isInfinite) {
+			len = 1.0f;
 		}
 
-		path.isTerminate = true;
-		//p[idx] = make_float4(contrib.x, contrib.y, contrib.z, 1);
-		surf2Dwrite(make_float4(contrib.x, contrib.y, contrib.z, 1), outSurface, ix * sizeof(float4), iy, cudaBoundaryModeTrap);
+		const auto c0 = max(0.0f, dot(orienting_normal, dirToLight));
+		float c1 = 1.0f;
+
+		if (!light.attrib.isSingular) {
+			c1 = max(0.0f, dot(sampleres.nml, -dirToLight));
+		}
+
+		auto G = c0 * c1 / (len * len);
+
+		path.throughput = path.throughput * (mtrl->baseColor * sampleres.finalColor) * G;
 	}
 }
+
+__global__ void hitShadowRay(
+	cudaSurfaceObject_t outSurface,
+	Path* paths,
+	ShadowRay* shadowRays,
+	int width, int height,
+	aten::ShapeParameter* shapes, int geomnum,
+	aten::MaterialParameter* mtrls,
+	aten::LightParameter* lights, int lightnum,
+	cudaTextureObject_t* nodes,
+	aten::PrimitiveParamter* prims,
+	cudaTextureObject_t vertices,
+	aten::mat4* matrices)
+{
+	const auto ix = blockIdx.x * blockDim.x + threadIdx.x;
+	const auto iy = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (ix >= width && iy >= height) {
+		return;
+	}
+
+	const auto idx = iy * width + ix;
+
+	auto& path = paths[idx];
+
+	if (path.isTerminate) {
+		return;
+	}
+
+	Context ctxt;
+	{
+		ctxt.geomnum = geomnum;
+		ctxt.shapes = shapes;
+		ctxt.mtrls = mtrls;
+		ctxt.lightnum = lightnum;
+		ctxt.lights = lights;
+		ctxt.nodes = nodes;
+		ctxt.prims = prims;
+		ctxt.vertices = vertices;
+		ctxt.matrices = matrices;
+	}
+
+	auto& shadowRay = shadowRays[idx];
+
+	if (shadowRay.isActive) {
+		aten::hitrecord rec;
+		bool isHit = intersectBVH(&ctxt, shadowRay, AT_MATH_EPSILON, AT_MATH_INF, &rec);
+
+		auto light = ctxt.lights[shadowRay.targetLightId];
+		if (light.object.idx >= 0) {
+			light.object.ptr = &ctxt.shapes[light.object.idx];
+		}
+
+		shadowRay.isActive = AT_NAME::scene::hitLight(
+			isHit,
+			&light,
+			shadowRay.lightPos,
+			shadowRay,
+			AT_MATH_EPSILON, AT_MATH_INF,
+			rec.p,
+			rec.t,
+			rec.obj);
+
+		if (shadowRay.isActive) {
+			path.isTerminate = true;
+
+			auto contrib = path.throughput;
+
+			//p[idx] = make_float4(contrib.x, contrib.y, contrib.z, 1);
+			surf2Dwrite(
+				make_float4(contrib.x, contrib.y, contrib.z, 1), outSurface, ix * sizeof(float4), iy, cudaBoundaryModeTrap);
+		}
+	}
+}
+
 
 namespace idaten {
 	void RayTracing::prepare()
@@ -263,6 +339,9 @@ namespace idaten {
 		}
 		nodetex.writeByNum(&tmp[0], tmp.size());
 
+		idaten::TypedCudaMemory<ShadowRay> shadowRays;
+		shadowRays.init(width * height);
+
 		genPathRayTracing << <grid, block >> > (
 			paths.ptr(),
 			width, height,
@@ -291,9 +370,9 @@ namespace idaten {
 			//checkCudaErrors(cudaDeviceSynchronize());
 
 			raytracing << <grid, block >> > (
-				//(float4*)dst.ptr(),
 				outputSurf,
 				paths.ptr(),
+				shadowRays.ptr(),
 				width, height,
 				shapeparam.ptr(), shapeparam.num(),
 				mtrlparam.ptr(),
@@ -306,6 +385,25 @@ namespace idaten {
 			err = cudaGetLastError();
 			if (err != cudaSuccess) {
 				AT_PRINTF("Cuda Kernel Err(raytracing) [%s]\n", cudaGetErrorString(err));
+			}
+
+			hitShadowRay << <grid, block >> > (
+				//hitShadowRay << <1, 1 >> > (
+				outputSurf,
+				paths.ptr(),
+				shadowRays.ptr(),
+				width, height,
+				shapeparam.ptr(), shapeparam.num(),
+				mtrlparam.ptr(),
+				lightparam.ptr(), lightparam.num(),
+				nodetex.ptr(),
+				primparams.ptr(),
+				vtxTex,
+				mtxparams.ptr());
+
+			err = cudaGetLastError();
+			if (err != cudaSuccess) {
+				AT_PRINTF("Cuda Kernel Err(hitShadowRay) [%s]\n", cudaGetErrorString(err));
 			}
 
 			//checkCudaErrors(cudaDeviceSynchronize());

f024593f12e3f5b9917b1829ef9ce60e003c8d8c
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-09-19 11:15:51 +0900

    Modify computing bilateral weight.

--- src/libidaten/svgf/svgf_VarianceEstimation.cu
+++ src/libidaten/svgf/svgf_VarianceEstimation.cu
@@ -30,9 +30,50 @@ inline __device__ idaten::SVGFPathTracing::AOV* sampleAov(
 	return &aovs[idx];
 }
 
+inline __device__ float3 computeViewSpace(
+	int ix, int iy,
+	float centerDepth,
+	int width, int height,
+	const aten::mat4* mtxC2V)
+{
+	// NOTE
+	// Pview = (Xview, Yview, Zview, 1)
+	// mtxV2C = W 0 0  0
+	//          0 H 0  0
+	//          0 0 A  B
+	//          0 0 -1 0
+	// mtxV2C * Pview = (Xclip, Yclip, Zclip, Wclip) = (Xclip, Yclip, Zclip, Zview)
+	//  Wclip = Zview = depth
+	// Xscr = Xclip / Wclip = Xclip / Zview = Xclip / depth
+	// Yscr = Yclip / Wclip = Yclip / Zview = Yclip / depth
+	//
+	// Xscr * depth = Xclip
+	// Xview = mtxC2V * Xclip
+
+	float2 uv = make_float2(ix + 0.5, iy + 0.5);
+	uv /= make_float2(width - 1, height - 1);	// [0, 1]
+	uv = uv * 2.0f - 1.0f;	// [0, 1] -> [-1, 1]
+
+	aten::vec4 pos(uv.x, uv.y, 0, 0);
+
+	// Screen-space -> Clip-space.
+	pos.x *= centerDepth;
+	pos.y *= centerDepth;
+
+	// Clip-space -> View-space
+	pos = mtxC2V->apply(pos);
+	pos.z = -centerDepth;
+	pos.w = 1.0;
+
+	return make_float3(pos.x, pos.y, pos.z);
+}
+
+#define IS_IN_BOUND(x, a, b)	((a) <= (x) && (x) < (b))
+
 __global__ void varianceEstimation(
 	cudaSurfaceObject_t dst,
 	idaten::SVGFPathTracing::AOV* aovs,
+	aten::mat4 mtxC2V,
 	int width, int height)
 {
 	int ix = blockIdx.x * blockDim.x + threadIdx.x;
@@ -58,6 +99,8 @@ __global__ void varianceEstimation(
 			cudaBoundaryModeTrap);
 	}
 
+	auto centerViewPos = computeViewSpace(ix, iy, centerDepth, width, height, &mtxC2V);
+
 	float4 centerMoment = aovs[idx].moments;
 
 	int frame = (int)centerMoment.w;
@@ -74,7 +117,7 @@ __global__ void varianceEstimation(
 		static const int radius = 3;
 		static const float sigmaN = 0.005f;
 		static const float sigmaD = 0.005f;
-		static const float sigmaS = 8;
+		static const float sigmaS = 0.965f;
 
 		auto centerNormal = aovs[idx].normal;
 
@@ -85,29 +128,64 @@ __global__ void varianceEstimation(
 		{
 			for (int u = -radius; u <= radius; u++)
 			{
-				auto sampleaov = sampleAov(aovs, ix + u, iy + v, width, height);
-				
-				auto moment = sampleaov->moments;
-				moment /= moment.w;
+				if (IS_IN_BOUND(ix + u, 0, width)
+					&& IS_IN_BOUND(iy + v, 0, height))
+				{
+					auto sampleaov = sampleAov(aovs, ix + u, iy + v, width, height);
 
-				auto sampleNml = sampleaov->normal;
+					auto moment = sampleaov->moments;
+					moment /= moment.w;
 
-				float sampleDepth = sampleaov->depth;
-				int sampleMeshId = sampleaov->meshid;
+					auto sampleNml = sampleaov->normal;
 
-				float n = 1 - dot(sampleNml, centerNormal);
-				float Wn = exp(-0.5f * n * n / (sigmaN * sigmaN));
+					float sampleDepth = sampleaov->depth;
+					int sampleMeshId = sampleaov->meshid;
 
-				float d = 1 - min(centerDepth, sampleDepth) / max(centerDepth, sampleDepth);
-				float Wd = exp(-0.5f * d * d / (sigmaD * sigmaD));
+#if 0
+					float n = 1 - dot(sampleNml, centerNormal);
+					float Wn = exp(-0.5f * n * n / (sigmaN * sigmaN));
 
-				float Ws = exp(-0.5f * (u * u + v * v) / (sigmaS * sigmaS));
+					float d = 1 - min(centerDepth, sampleDepth) / max(centerDepth, sampleDepth);
+					float Wd = exp(-0.5f * d * d / (sigmaD * sigmaD));
 
-				float Wm = centerMeshId == sampleMeshId ? 1.0f : 0.0f;
+					float Ws = exp(-0.5f * (u * u + v * v) / (sigmaS * sigmaS));
+#else
+					float Wn = 1.0f;
+					{
+						float normalCloseness = dot(sampleNml, centerNormal);
+						normalCloseness = normalCloseness * normalCloseness;
+						normalCloseness = normalCloseness * normalCloseness;
+						float normalError = (1.0f - normalCloseness);
+						Wn = max((1.0f - normalError), 0.0f);
+					}
 
-				float W = Ws * Wn * Wd * Wm;
-				sum += moment * W;
-				weight += W;
+					float Wd = max(0.0f, 1.0f - abs(centerDepth - sampleDepth));
+
+					float Ws = 1.0f;
+					{
+						auto sampleViewPos = computeViewSpace(ix + u, iy + v, sampleDepth, width, height, &mtxC2V);
+
+						// Change in position in camera space.
+						auto dq = centerViewPos - sampleViewPos;
+
+						// How far away is this point from the original sample in camera space? (Max value is unbounded).
+						auto dist2 = dot(dq, dq);
+
+						// How far off the expected plane (on the perpendicular) is this point?  Max value is unbounded.
+						float err = max(abs(dot(dq, sampleNml)), abs(dot(dq, centerNormal)));
+
+						Ws = (dist2 < 0.001f)
+							? 1.0
+							: pow(max(0.0, 1.0 - 2.0 * err / sqrt(dist2)), 2.0);
+					}
+#endif
+
+					float Wm = centerMeshId == sampleMeshId ? 1.0f : 0.0f;
+
+					float W = Ws * Wn * Wd * Wm;
+					sum += moment * W;
+					weight += W;
+				}
 			}
 		}
 
@@ -148,6 +226,7 @@ namespace idaten
 		//varianceEstimation << <1, 1 >> > (
 			outputSurf,
 			curaov.ptr(),
+			m_mtxC2V,
 			width, height);
 
 		checkCudaKernel(varianceEstimation);

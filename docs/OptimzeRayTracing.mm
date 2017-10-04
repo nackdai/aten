e2e59279a089d9440aec6587c6009084a379143b
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-10-04 18:30:29 +0900

    Add a mindmap.

---
+++ docs/OptimzeRayTracing.mm
@@ -0,0 +1,76 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1507091972806" ID="ID_639959707" MODIFIED="1507091980811" TEXT="OptimzeRT">
+<node CREATED="1507092217426" ID="ID_39119329" MODIFIED="1507107714113" POSITION="right" TEXT="&#x53c2;&#x8003;&#x30ea;&#x30f3;&#x30af;">
+<node CREATED="1507092215119" ID="ID_568942259" LINK="http://raytracey.blogspot.jp/2016/09/gpu-path-tracing-tutorial-4-optimised.html" MODIFIED="1507092215119" TEXT="raytracey.blogspot.jp &gt; 2016 &gt; 09 &gt; Gpu-path-tracing-tutorial-4-optimised"/>
+<node CREATED="1507092237905" ID="ID_1927465354" LINK="http://robbinmarcus.blogspot.jp/2015/11/real-time-raytracing-part-3.html" MODIFIED="1507092237905" TEXT="robbinmarcus.blogspot.jp &gt; 2015 &gt; 11 &gt; Real-time-raytracing-part-3"/>
+<node CREATED="1507092278227" LINK="http://robbinmarcus.blogspot.jp/2015/12/real-time-raytracing-part-31.html" MODIFIED="1507092278227" TEXT="robbinmarcus.blogspot.jp &gt; 2015 &gt; 12 &gt; Real-time-raytracing-part-31"/>
+<node CREATED="1507107675995" LINK="http://research.nvidia.com/sites/default/files/publications/aila2009hpg_paper.pdf" MODIFIED="1507107675995" TEXT="research.nvidia.com &gt; Sites &gt; Default &gt; Files &gt; Publications &gt; Aila2009hpg paper"/>
+<node CREATED="1507107684996" LINK="http://citeseerx.ist.psu.edu/viewdoc/download" MODIFIED="1507107684996" TEXT="citeseerx.ist.psu.edu &gt; Viewdoc &gt; Download;jsessionid=E7AE35FB9EABFED6A43CE590E409AAB1 ? ..."/>
+<node CREATED="1507107704329" LINK="https://graphics.stanford.edu/~boulos/papers/togbvh.pdf" MODIFIED="1507107704329" TEXT="https://graphics.stanford.edu/~boulos/papers/togbvh.pdf"/>
+<node CREATED="1507107714569" LINK="http://gamma.cs.unc.edu/HART/HART_files/HART_preprint.pdf" MODIFIED="1507107714569" TEXT="gamma.cs.unc.edu &gt; HART &gt; HART files &gt; HART preprint"/>
+<node CREATED="1507107724552" LINK="http://jcgt.org/published/0004/03/02/paper.pdf" MODIFIED="1507107724552" TEXT="jcgt.org &gt; Published &gt; 0004 &gt; 03 &gt; 02 &gt; Paper"/>
+</node>
+<node CREATED="1507092246382" ID="ID_449402318" MODIFIED="1507092248379" POSITION="right" TEXT="TODO">
+<node CREATED="1507092248381" ID="ID_1848368983" MODIFIED="1507092250211" TEXT="Memory alignment">
+<node CREATED="1507092250768" ID="ID_603609798" MODIFIED="1507092265114" TEXT="128bit&#xff08;16byte&#xff09;&#x30a2;&#x30e9;&#x30a4;&#x30f3;&#x30e1;&#x30f3;&#x30c8;&#xff06;&#x30a2;&#x30af;&#x30bb;&#x30b9;">
+<node CREATED="1507092237905" ID="ID_515367395" LINK="http://robbinmarcus.blogspot.jp/2015/11/real-time-raytracing-part-3.html" MODIFIED="1507092237905" TEXT="robbinmarcus.blogspot.jp &gt; 2015 &gt; 11 &gt; Real-time-raytracing-part-3"/>
+</node>
+</node>
+<node CREATED="1507092266231" ID="ID_1544996274" MODIFIED="1507098327651" TEXT="cast">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1507092380432" ID="ID_1934221446" MODIFIED="1507098319469" TEXT="__float2int_rn"/>
+<node CREATED="1507098321856" ID="ID_1149899268" MODIFIED="1507098324484" TEXT="&#x52b9;&#x679c;&#x306a;&#x3057;"/>
+</node>
+<node CREATED="1507093950080" ID="ID_686067702" MODIFIED="1507096210144" TEXT="persistent thread">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1507093954944" ID="ID_1197948158" MODIFIED="1507093973883" TEXT="sponza&#x3060;&#x3068;ON/OFF&#x3067;&#x7d04;5ms&#x9055;&#x3046;"/>
+</node>
+<node CREATED="1507098342202" ID="ID_98076709" MODIFIED="1507098356370" TEXT="Woop ray/triangle intersection"/>
+<node CREATED="1507093950080" ID="ID_1293127889" MODIFIED="1507102184499" TEXT="persistent thread">
+<node CREATED="1507109043454" ID="ID_1619109723" MODIFIED="1507109056226" TEXT="shade&#x51e6;&#x7406;&#x3067;&#x306f;&#x52b9;&#x679c;&#x306a;&#x3057;&#x3001;&#x9006;&#x306b;&#x9045;&#x304f;&#x306a;&#x308b;"/>
+<node CREATED="1507102187648" ID="ID_1390490193" MODIFIED="1507109064665" TEXT="shadow ray &#x306e; hit test &#x3092;&#x5206;&#x96e2;&#x3057;&#x3066; persitent thread &#x3067;&#x51e6;&#x7406;&#x3059;&#x308b;">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+</node>
+<node CREATED="1507109127782" ID="ID_1629311417" MODIFIED="1507109270295" POSITION="right" TEXT="&#x8981;&#x691c;&#x8a0e;&#x30e1;&#x30e2;">
+<node CREATED="1507109122412" ID="ID_401997312" MODIFIED="1507109125168" TEXT="LOD"/>
+<node CREATED="1507109149757" ID="ID_428371364" MODIFIED="1507109156472" TEXT="&#x9060;&#x65b9;&#x3092;&#x7701;&#x304f;">
+<node CREATED="1507109166958" ID="ID_1044912312" MODIFIED="1507109167649" TEXT="&#x9060;&#x65b9;&#x306e;&#x30d0;&#x30a6;&#x30f3;&#x30b9;&#x6570;&#x3092;&#x6e1b;&#x3089;&#x3059; "/>
+<node CREATED="1507109173173" ID="ID_837714105" MODIFIED="1507109178984" TEXT="&#x9060;&#x65b9;&#x3067;&#x306f;&#x8907;&#x96d1;&#x306a;BRDF&#x3092;&#x4f7f;&#x308f;&#x306a;&#x3044;">
+<node CREATED="1507109179939" MODIFIED="1507109179939" TEXT="&#x30de;&#x30c6;&#x30ea;&#x30a2;&#x30eb;&#x306e;LOD"/>
+</node>
+<node CREATED="1507109180677" ID="ID_145630843" MODIFIED="1507109186423" TEXT="&#x9060;&#x65b9;&#x306e;&#x4ea4;&#x5dee;&#x5224;&#x5b9a;&#x306f;LOD ">
+<node CREATED="1507109208010" MODIFIED="1507109208010" TEXT="&#x30dc;&#x30af;&#x30bb;&#x30eb;"/>
+<node CREATED="1507109215928" MODIFIED="1507109215928" TEXT="&#x5149;&#x7dda;&#x306e;&#x5143;&#x304c;ideal specular&#x3060;&#x3068;&#x307e;&#x305a;&#x3044;"/>
+</node>
+</node>
+<node CREATED="1507109195621" ID="ID_1207276363" MODIFIED="1507109196168" TEXT="&#x30ea;&#x30d5;&#x30ec;&#x30af;&#x30b7;&#x30e7;&#x30f3;&#x3092;&#x7701;&#x304f; ">
+<node CREATED="1507109224080" ID="ID_1515937876" MODIFIED="1507109224080" TEXT="Glossy&#x306e;&#x30ea;&#x30d5;&#x30ec;&#x30af;&#x30b7;&#x30e7;&#x30f3;&#x306a;&#x3089;&#x6b63;&#x78ba;&#x3067;&#x306a;&#x304f;&#x3066;&#x3082;&#x6c17;&#x3065;&#x304b;&#x306a;&#x3044;&#x201d;&#x306f;&#x305a;&#x201d;">
+<node CREATED="1507109228862" MODIFIED="1507109228862" TEXT="&#x30d0;&#x30a6;&#x30f3;&#x30b9;&#x6570;&#x3092;&#x6e1b;&#x3089;&#x3059;"/>
+<node CREATED="1507109234062" ID="ID_809500692" MODIFIED="1507109234062" TEXT="&#x8907;&#x96d1;&#x306a;BRDF&#x3092;&#x4f7f;&#x308f;&#x306a;&#x3044;">
+<node CREATED="1507109369164" ID="ID_234983067" MODIFIED="1507109382559" TEXT="&#x5f37;&#x5236;&#x7684;&#x306b;Lambert&#x306b;&#x3059;&#x308b;"/>
+</node>
+<node CREATED="1507109238992" MODIFIED="1507109238992" TEXT="&#x30e9;&#x30d5;&#x30cd;&#x30b9;&#x304c;&#x8352;&#x304f;&#x306a;&#x3044;&#x5834;&#x5408;&#x3092;&#x8003;&#x3048;&#x306a;&#x3044;&#x3068;&#x3044;&#x3051;&#x306a;&#x3044;"/>
+</node>
+<node CREATED="1507109249869" MODIFIED="1507109249869" TEXT="Ideal specular &#x3060;&#x3068;&#x7701;&#x304f;&#x3068;&#x307e;&#x305a;&#x305d;&#x3046;&#x3060;&#x304c;"/>
+</node>
+<node CREATED="1507109255945" MODIFIED="1507109255945" TEXT="&#x540c;&#x3058;&#x7269;&#x4f53;&#x9593;&#x306e;&#x30ec;&#x30a4;&#x306e;&#x53cd;&#x5fa9;&#x306f;&#x6253;&#x3061;&#x5207;&#x308b;"/>
+<node CREATED="1507109277793" ID="ID_510505007" MODIFIED="1507109277793" TEXT="&#x30e9;&#x30d5;&#x30cd;&#x30b9;&#x3092;&#x8352;&#x304f;&#x3057;&#x3066;&#x3044;&#x304f;">
+<node CREATED="1507109281811" MODIFIED="1507109281811" TEXT="Glossy&#x306a;&#x30de;&#x30c6;&#x30ea;&#x30a2;&#x30eb;&#x9593;&#x306e;&#x30d0;&#x30a6;&#x30f3;&#x30b9;&#x306a;&#x3089;&#x30e9;&#x30d5;&#x30cd;&#x30b9;&#x3092;&#x8352;&#x304f;&#x3059;&#x308b;"/>
+<node CREATED="1507109286157" MODIFIED="1507109286157" TEXT="&#x30ed;&#x30b7;&#x30a2;&#x30f3;&#x30eb;&#x30fc;&#x30ec;&#x30c3;&#x30c8;&#x3067;&#x65e9;&#x304f;&#x6253;&#x3061;&#x5207;&#x3089;&#x308c;&#x308b;&#x53ef;&#x80fd;&#x6027;&#x3082;&#x9ad8;&#x304f;&#x306a;&#x308b;"/>
+</node>
+<node CREATED="1507109292705" ID="ID_954072622" MODIFIED="1507109292705" TEXT="&#x30af;&#x30e9;&#x30f3;&#x30d7;&#x3059;&#x308b;">
+<node CREATED="1507109296408" ID="ID_1731998613" MODIFIED="1507109296408" TEXT="&#x4e00;&#x5b9a;&#x5024;&#x4ee5;&#x4e0b;&#x306b;&#x306a;&#x3063;&#x305f;&#x3089;&#x6253;&#x3061;&#x5207;&#x308b;">
+<node CREATED="1507109298036" ID="ID_1809766065" MODIFIED="1507109306719" TEXT="&#x30ed;&#x30b7;&#x30a2;&#x30f3;&#x30eb;&#x30fc;&#x30ec;&#x30c3;&#x30c8;">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1507109311396" ID="ID_1174414119" MODIFIED="1507109311396" TEXT="&#x4e00;&#x5b9a;&#x5024;&#x4ee5;&#x4e0a;&#x306b;&#x306a;&#x3089;&#x306a;&#x3044;&#x3088;&#x3046;&#x306b;&#x3059;&#x308b;">
+<node CREATED="1507109314687" MODIFIED="1507109314687" TEXT="&#x30d5;&#x30a1;&#x30a4;&#x30a2;&#x30d5;&#x30e9;&#x30a4;&#x5bfe;&#x7b56;"/>
+</node>
+</node>
+</node>
+</node>
+</map>

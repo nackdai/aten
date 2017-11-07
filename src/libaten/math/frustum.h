174a55fe01a1e9371bb4304a8365c51078e60148
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-11-07 16:55:57 +0900

    Implement hit test between frustum and aabb.

--- src/libaten/math/frustum.h
+++ src/libaten/math/frustum.h
@@ -5,6 +5,9 @@
 #include "math/mat4.h"
 #include "math/aabb.h"
 
+#pragma optimize( "", off)
+
+
 namespace aten {
 	class frustum {
 	public:
@@ -16,11 +19,16 @@ namespace aten {
 			const vec3& org,
 			const vec3 dir[4])
 		{
+			// NOTE
+			// 1--2
+			// |  |
+			// 0--3
+
 			// normal.
-			m_plane[0] = vec4(cross(dir[0], dir[1]), 0.0f);
-			m_plane[1] = vec4(cross(dir[1], dir[2]), 0.0f);
-			m_plane[2] = vec4(cross(dir[2], dir[3]), 0.0f);
-			m_plane[3] = vec4(cross(dir[3], dir[0]), 0.0f);
+			m_plane[0] = vec4(normalize(cross(dir[1], dir[0])), 0.0f);
+			m_plane[1] = vec4(normalize(cross(dir[2], dir[1])), 0.0f);
+			m_plane[2] = vec4(normalize(cross(dir[3], dir[2])), 0.0f);
+			m_plane[3] = vec4(normalize(cross(dir[0], dir[3])), 0.0f);
 
 			// offset.
 			m_plane[0].w = dot(org, vec3(m_plane[0]));
@@ -32,10 +40,10 @@ namespace aten {
 			m_points[0] = org;
 
 			// TODO
-			m_points[1] = org + dir[0] * real(100000.0);
-			m_points[2] = org + dir[1] * real(100000.0);
-			m_points[3] = org + dir[2] * real(100000.0);
-			m_points[4] = org + dir[3] * real(100000.0);
+			m_points[1] = org + dir[0] * real(10000.0);
+			m_points[2] = org + dir[1] * real(10000.0);
+			m_points[3] = org + dir[2] * real(10000.0);
+			m_points[4] = org + dir[3] * real(10000.0);
 
 			m_dir[0] = dir[0];
 			m_dir[1] = dir[1];
@@ -49,10 +57,10 @@ namespace aten {
 
 			org = mtx.apply(org);
 
-			m_dir[0] = mtx.applyXYZ(m_dir[0]);
-			m_dir[1] = mtx.applyXYZ(m_dir[1]);
-			m_dir[2] = mtx.applyXYZ(m_dir[2]);
-			m_dir[3] = mtx.applyXYZ(m_dir[3]);
+			m_dir[0] = normalize(mtx.applyXYZ(m_dir[0]));
+			m_dir[1] = normalize(mtx.applyXYZ(m_dir[1]));
+			m_dir[2] = normalize(mtx.applyXYZ(m_dir[2]));
+			m_dir[3] = normalize(mtx.applyXYZ(m_dir[3]));
 
 			update(org, m_dir);
 		}
@@ -63,47 +71,51 @@ namespace aten {
 			Intersecting,
 		};
 
-		Intersect intersect(const aabb& box) const
+		bool intersect(const aabb& box) const
 		{
 			return intersect(box.minPos(), box.maxPos());
 		}
 
-		Intersect intersect(const vec3& minbox, const vec3& maxbox) const
+		bool intersect(const vec3& minbox, const vec3& maxbox) const
 		{
 			// NOTE
 			// http://www.txutxi.com/?p=584
-
-			// TODO
-			// SIMDfy...
-
-			bool intersecting = false;
+			// http://old.cescg.org/CESCG-2002/DSykoraJJelinek/
+			// https://www.gamedev.net/forums/topic/672043-perfect-aabb-frustum-intersection-test/
 
 			vec3 b[] = { minbox, maxbox };
 
+			// Inside.
+			bool intersecting = false;
+
 			for (int i = 0; i < AT_COUNTOF(m_plane); i++) {
-				const vec4& plane = m_plane[i];
+				const vec4& p = m_plane[i];
 
-				int px = (plane.x > real(0.0));
-				int py = (plane.y > real(0.0));
-				int pz = (plane.z > real(0.0));
+				int px = (p.x > real(0.0));
+				int py = (p.y > real(0.0));
+				int pz = (p.z > real(0.0));
 
-				// Dot product
-				// project p-vertex on plane normal
-				// (How far is p-vertex from the origin)
-				real d = (plane.x * b[px].x)
-					+ (plane.y * b[py].y)
-					+ (plane.z * b[py].z);
+				vec3 vmax(b[px].x, b[py].y, b[pz].z);
+				vec3 vmin(b[1 - px].x, b[1 - py].y, b[1 - pz].z);
 
-				if (d < -plane.w) {
-					return Intersect::Miss;
-				}
-				else {
+				auto d0 = dot((vec3)p, vmax);
+				auto d1 = dot((vec3)p, vmin);
+
+				if (d1 <= p.w && p.w <= d0) {
+					// Intersecting.
 					intersecting = true;
+				}
+				else if (d1 > p.w) {
+					// Miss.
+					return false;
 				}
 			}
 
 			// NOTE
 			// https://cesium.com/blog/2017/02/02/tighter-frustum-culling-and-why-you-may-want-to-disregard-it/
+
+			// TODO
+			// ‚±‚Ì•û–@‚Å‚Í intersecting‚Æinside‚Ìˆá‚¢‚ð”»’è‚Å‚«‚È‚¢‚ÆŽv‚í‚ê‚é...
 
 			if (intersecting) {
 				auto center = (minbox + maxbox) * real(0.5);
@@ -128,15 +140,19 @@ namespace aten {
 				};
 
 				for (int i = 0; i < 3; i++) {
-					const auto& a = axis[3];
+					const auto& a = axis[i];
 					
 					auto axisLengthSquared = squared_length(a);
 
 					int out1 = 0;
 					int out2 = 0;
 
+					if (axisLengthSquared == real(0)) {
+						continue;
+					}
+
 					for (int n = 0; n < AT_COUNTOF(diff); n++) {
-						auto proj = dot(diff[i], a);
+						auto proj = dot(diff[n], a);
 
 						if (proj >= axisLengthSquared) {
 							out1++;
@@ -147,14 +163,16 @@ namespace aten {
 					}
 
 					if (out1 == AT_COUNTOF(diff) || out2 == AT_COUNTOF(diff)) {
-						return Intersect::Miss;
+						//return Intersect::Miss;
+						return false;
 					}
 
 					intersecting |= (out1 != 0 || out2 != 0);
 				}
 			}
 
-			return intersecting ? Intersect::Intersecting : Intersect::Inside;
+			//return intersecting ? Intersect::Intersecting : Intersect::Inside;
+			return true;
 		}
 
 	private:
@@ -169,3 +187,5 @@ namespace aten {
 		vec3 m_dir[4];
 	};
 }
+
+#pragma optimize( "", on)

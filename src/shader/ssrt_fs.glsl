d7b4551e6b0384329703a6094ff01c7653df6f3f
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-11-09 13:28:43 +0900

    It is enabld to compute bary-centic in shader.

--- src/shader/ssrt_fs.glsl
+++ src/shader/ssrt_fs.glsl
@@ -4,11 +4,15 @@ precision highp int;
 
 in vec3 normal;
 in vec2 uv;
+in vec3 baryCentric;
 
 layout(location = 0) out vec4 outColor;
 
 void main()
 {
-	outColor.xyz = normal * 0.5 + 0.5;
+	//outColor.xyz = normal * 0.5 + 0.5;
+	
+	outColor.xyz = baryCentric;
+
 	outColor.w = 1.0;
 }
--- src/shader/ssrt_gs.glsl
+++ src/shader/ssrt_gs.glsl
@@ -13,13 +13,28 @@ in vec2 vUV[3];
 
 out vec3 normal;
 out vec2 uv;
+out vec3 baryCentric;
+
+
+// TODO
+// パストレ内の計算方法と合うように順番を対応させているので汎用性に関しては要検討.
+
+// For computing bary centric.
+const vec3 weight[3] = {
+	vec3(0, 0, 1),
+	vec3(1, 0, 0),
+	vec3(0, 1, 0),
+};
 
 void main()
 {
 	for (int i = 0; i < gl_in.length(); i++) {
 		gl_Position = mtxW2C * gl_in[i].gl_Position;
+		
 		normal = worldNormal[i];
 		uv = vUV[i];
+		baryCentric = weight[i];
+
 		EmitVertex();
 	}
 	EndPrimitive();

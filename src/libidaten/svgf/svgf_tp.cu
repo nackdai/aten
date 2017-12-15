b93b5c941c63a06f89063cf311503bf767e111bc
nackdai <newvf91.brjtn579.dn@gmail.com>
2017-12-15 14:40:00 +0900

    It is enabled to sample bilinear.

--- src/libidaten/svgf/svgf_tp.cu
+++ src/libidaten/svgf/svgf_tp.cu
@@ -70,6 +70,35 @@ inline __device__ void computePrevScreenPos(
 	*prevPos = *prevPos * 0.5 + 0.5;	// [-1, 1] -> [0, 1]
 }
 
+inline __device__ int getLinearIdx(int x, int y, int w, int h)
+{
+	int max_buffer_size = w * h;
+	return clamp(y * w + x, 0, max_buffer_size - 1);
+}
+
+// Bilinear sampler
+inline __device__ float4 sampleBilinear(
+	const float4* buffer, 
+	float uvx, float uvy,
+	int w, int h)
+{
+	float2 uv = make_float2(uvx, uvy) * make_float2(w, h) - make_float2(0.5f, 0.5f);
+
+	int x = floor(uv.x);
+	int y = floor(uv.y);
+
+	float2 uv_ratio = uv - make_float2(x, y);
+	float2 uv_inv = make_float2(1.f, 1.f) - uv_ratio;
+
+	int x1 = clamp(x + 1, 0, w - 1);
+	int y1 = clamp(y + 1, 0, h - 1);
+
+	float4 r = (buffer[getLinearIdx(x, y, w, h)] * uv_inv.x + buffer[getLinearIdx(x1, y, w, h)] * uv_ratio.x) * uv_inv.y +
+		(buffer[getLinearIdx(x, y1, w, h)] * uv_inv.x + buffer[getLinearIdx(x1, y1, w, h)] * uv_ratio.x) * uv_ratio.y;
+
+	return r;
+}
+
 __global__ void temporalReprojection(
 	const idaten::SVGFPathTracing::Path* __restrict__ paths,
 	const aten::CameraParameter* __restrict__ camera,
@@ -184,7 +213,8 @@ __global__ void temporalReprojection(
 				float Wm = centerMeshId == prevMeshId ? 1.0f : 0.0f;
 
 				// 前のフレームのピクセルカラーを取得.
-				float4 prev = prevAovColorVariance[pidx];
+				//float4 prev = prevAovColorVariance[pidx];
+				float4 prev = sampleBilinear(prevAovColorVariance, prevPos.x, prevPos.y, width, height);
 
 				float W = Wz * Wn * Wm;
 				sum += prev * W;

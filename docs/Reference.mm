4089ea9d347ff7f2797725ce77828b74d51c461f
nackdai <newvf91.brjtn579.dn@gmail.com>
2018-02-09 18:29:15 +0900

    Update mind map.

--- docs/Reference.mm
+++ docs/Reference.mm
@@ -9,5 +9,12 @@
 <node CREATED="1518165598074" ID="ID_1843010040" LINK="http://blog.mikepan.com/post/137759885931/realistic-car-paint-brdf-material" MODIFIED="1518165606611" TEXT="Realistic Car Paint BRDF Material"/>
 <node CREATED="1518165578371" ID="ID_727209115" LINK="https://knowledge.autodesk.com/ja/support/3ds-max/learn-explore/caas/CloudHelp/cloudhelp/2017/JPN/3DSMax/files/GUID-1CD21856-588A-4A05-AC0A-88489F5F9C84-htm.html" MODIFIED="1518165587203" TEXT="Car Paint &#x30de;&#x30c6;&#x30ea;&#x30a2;&#x30eb;"/>
 </node>
+<node CREATED="1518168230245" ID="ID_1061911945" MODIFIED="1518168232893" POSITION="right" TEXT="Environment map">
+<node CREATED="1518168236281" ID="ID_1242231935" LINK="http://ube.ege.edu.tr/~kurt/Publications/SCCG_2012_Ergun.pdf" MODIFIED="1518168246519" TEXT="Real-time Kd-tree Based Importance Sampling of Environment Maps"/>
+<node CREATED="1518168382496" ID="ID_1426186454" LINK="https://developer.nvidia.com/gpugems/GPUGems3/gpugems3_ch20.html" MODIFIED="1518168395290" TEXT="GPU-Based Importance Sampling">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1518168405377" ID="ID_79677346" LINK="https://vision.cornell.edu/se3/wp-content/uploads/2014/09/54_structured.pdf" MODIFIED="1518168418420" TEXT="Structured Importance Sampling of Environment Maps"/>
+</node>
 </node>
 </map>

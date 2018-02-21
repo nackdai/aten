a33091263b9ccae4bcc5648a605c604b9573815c
nackdai <newvf91.brjtn579.dn@gmail.com>
2018-02-21 17:39:15 +0900

    Add main.cpp, specify library and command to copy dll.

---
+++ src/FbxConverter/main.cpp
@@ -0,0 +1,223 @@
+#include "aten.h"
+#include "atenscene.h"
+
+#include <cmdline.h>
+#include <imgui.h>
+
+static const int WIDTH = 1280;
+static const int HEIGHT = 720;
+
+static const char* TITLE = "FbxConverter";
+
+struct Options {
+	std::string input;
+	std::string output;
+
+	std::string inputBasepath;
+	std::string inputFilename;
+} g_opt;
+
+static aten::PinholeCamera g_camera;
+static bool g_isCameraDirty = false;
+
+static uint32_t g_lodTriCnt = 0;
+static bool g_isWireFrame = true;
+static bool g_isUpdateBuffer = false;
+static bool g_displayLOD = true;
+
+static bool g_willShowGUI = true;
+
+static bool g_isMouseLBtnDown = false;
+static bool g_isMouseRBtnDown = false;
+static int g_prevX = 0;
+static int g_prevY = 0;
+
+void onRun()
+{
+}
+
+void onClose()
+{
+
+}
+
+void onMouseBtn(bool left, bool press, int x, int y)
+{
+	g_isMouseLBtnDown = false;
+	g_isMouseRBtnDown = false;
+
+	if (press) {
+		g_prevX = x;
+		g_prevY = y;
+
+		g_isMouseLBtnDown = left;
+		g_isMouseRBtnDown = !left;
+	}
+}
+
+void onMouseMove(int x, int y)
+{
+	if (g_isMouseLBtnDown) {
+		aten::CameraOperator::rotate(
+			g_camera,
+			WIDTH, HEIGHT,
+			g_prevX, g_prevY,
+			x, y);
+		g_isCameraDirty = true;
+	}
+	else if (g_isMouseRBtnDown) {
+		aten::CameraOperator::move(
+			g_camera,
+			g_prevX, g_prevY,
+			x, y,
+			real(0.001));
+		g_isCameraDirty = true;
+	}
+
+	g_prevX = x;
+	g_prevY = y;
+}
+
+void onMouseWheel(int delta)
+{
+	aten::CameraOperator::dolly(g_camera, delta * real(0.1));
+	g_isCameraDirty = true;
+}
+
+void onKey(bool press, aten::Key key)
+{
+	static const real offset = real(0.1);
+
+	if (press) {
+		if (key == aten::Key::Key_F1) {
+			g_willShowGUI = !g_willShowGUI;
+			return;
+		}
+	}
+
+	if (press) {
+		switch (key) {
+		case aten::Key::Key_W:
+		case aten::Key::Key_UP:
+			aten::CameraOperator::moveForward(g_camera, offset);
+			break;
+		case aten::Key::Key_S:
+		case aten::Key::Key_DOWN:
+			aten::CameraOperator::moveForward(g_camera, -offset);
+			break;
+		case aten::Key::Key_D:
+		case aten::Key::Key_RIGHT:
+			aten::CameraOperator::moveRight(g_camera, offset);
+			break;
+		case aten::Key::Key_A:
+		case aten::Key::Key_LEFT:
+			aten::CameraOperator::moveRight(g_camera, -offset);
+			break;
+		case aten::Key::Key_Z:
+			aten::CameraOperator::moveUp(g_camera, offset);
+			break;
+		case aten::Key::Key_X:
+			aten::CameraOperator::moveUp(g_camera, -offset);
+			break;
+		default:
+			break;
+		}
+
+		g_isCameraDirty = true;
+	}
+}
+
+bool parseOption(
+	int argc, char* argv[],
+	cmdline::parser& cmd,
+	Options& opt)
+{
+	{
+		cmd.add<std::string>("input", 'i', "input filename", true);
+		cmd.add<std::string>("output", 'o', "output filename base", false, "result");
+
+		cmd.add<std::string>("help", '?', "print usage", false);
+	}
+
+	bool isCmdOk = cmd.parse(argc, argv);
+
+	if (cmd.exist("help")) {
+		std::cerr << cmd.usage();
+		return false;
+	}
+
+	if (!isCmdOk) {
+		std::cerr << cmd.error() << std::endl << cmd.usage();
+		return false;
+	}
+
+	if (cmd.exist("input")) {
+		opt.input = cmd.get<std::string>("input");
+	}
+	else {
+		std::cerr << cmd.error() << std::endl << cmd.usage();
+		return false;
+	}
+
+	if (cmd.exist("output")) {
+		opt.output = cmd.get<std::string>("output");
+	}
+	else {
+		// TODO
+		opt.output = "result.sbvh";
+	}
+
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	g_opt.input = "../../asset/sponza/sponza.obj";
+	//g_opt.input = "../../asset/sponza/lod.obj";
+	//g_opt.input = "../../asset/suzanne/suzanne.obj";
+
+	// TODO
+#if 0
+	cmdline::parser cmd;
+
+	if (!parseOption(argc, argv, cmd, g_opt)) {
+		return 0;
+	}
+#endif
+
+	std::string extname;
+	aten::getStringsFromPath(g_opt.input, g_opt.inputBasepath, extname, g_opt.inputFilename);
+
+	aten::window::SetCurrentDirectoryFromExe();
+
+
+
+#if 0
+	aten::window::init(
+		WIDTH, HEIGHT,
+		TITLE,
+		onClose,
+		onMouseBtn,
+		onMouseMove,
+		onMouseWheel,
+		onKey);
+
+	// TODO
+	aten::vec3 pos(0, 1, 10);
+	aten::vec3 at(0, 1, 1);
+	real vfov = real(45);
+
+	g_camera.init(
+		pos,
+		at,
+		aten::vec3(0, 1, 0),
+		vfov,
+		WIDTH, HEIGHT);
+
+	aten::window::run(onRun);
+
+	aten::window::terminate();
+#endif
+
+	return 1;
+}

50bc71705540a498b09112b83796a2a9fc179123
nackdai <newvf91.brjtn579.dn@gmail.com>
2018-04-17 17:18:46 +0900

    Fix an error that files for implementation are wrong...

--- src/libaten/os/windows/system_windows.cpp
+++ src/libaten/os/windows/system_windows.cpp
@@ -1,65 +1,29 @@
-#include <unistd.h>
-#include <string.h>
 #include "os/system.h"
+#include <Shlwapi.h>
 
 namespace aten
 {
-	static uint64_t ptrToInt(void* p)
-	{
-		AT_STATICASSERT(sizeof(uint64_t) >= sizeof(void*));
-
-		union {
-			uint64_t val;
-			void* ptr;
-		} conv;
-
-		conv.ptr = p;
-		return conv.val;
-	}
-
-	static inline uint64_t getPtrDistance(void* p0, void* p1)
-	{
-		uint64_t pos_0 = ptrToInt(p0);
-		uint64_t pos_1 = ptrToInt(p1);
-
-		uint64_t _max = std::max(pos_0, pos_1);
-		uint64_t _min = std::min(pos_0, pos_1);
-
-		return (_max - _min);
-	}
-
-	static inline int32_t getExecuteFilePath(char* path, size_t pathBufSize)
-	{
-		// 実行プログラムのフルパスを取得
-		int32_t result = readlink("/proc/self/exe", path, pathBufSize);
-		AT_VRETURN(result > 0, 0);
-
-		return result;
-	}
-
 	bool SetCurrentDirectoryFromExe()
 	{
-		static char path[256];
+		static char buf[_MAX_PATH];
 
 		// 実行プログラムのフルパスを取得
-		int pathSize = getExecuteFilePath(path, sizeof(path));
-		AT_VRETURN(pathSize > 0);
-
-		char* tmp = const_cast<char*>(path);
+		{
+			DWORD result = ::GetModuleFileName(
+				NULL,
+				buf,
+				sizeof(buf));
+			AT_ASSERT(result > 0);
+		}
 
 		// ファイル名を取り除く
-		char* p = ::strrchr(tmp, '/');
-		AT_VRETURN(p != nullptr);
-
-		auto diff = getPtrDistance(tmp, p);
-
-		tmp[diff] = 0;
-		p = tmp;
+		auto result = ::PathRemoveFileSpec(buf);
+		AT_ASSERT(result);
 
 		// カレントディレクトリを設定
-		int result = ::chdir(p);
-		AT_ASSERT(result >= 0);
+		result = ::SetCurrentDirectory(buf);
+		AT_ASSERT(result);
 
-		return result >= 0;
+		return result ? true : false;
 	}
 }

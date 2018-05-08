80df5585c1d51be90ecb3eae7b02de16ffeefc26
nackdai <newvf91.brjtn579.dn@gmail.com>
2018-05-08 11:55:31 +0900

    Apply for new api.

--- src/multigputest/main.cpp
+++ src/multigputest/main.cpp
@@ -427,7 +427,7 @@ int main()
 			lightparams,
 			nodes,
 			primparams,
-			vtxparams,
+			vtxparams, 0,
 			mtxs,
 			tex, idaten::EnvmapResource(envmap->id(), ibl.getAvgIlluminace(), real(1)));
 

1b12b8b68e80315bcb184ab59b6ac3177fd2c79f
nackdai <newvf91.brjtn579.dn@gmail.com>
2018-10-22 21:01:22 +0900

    Indent.

--- src/libaten/scene/context.h
+++ src/libaten/scene/context.h
@@ -11,100 +11,100 @@
 
 namespace aten
 {
-class context {
-public:
-    context() {}
-    virtual ~context() {}
+    class context {
+    public:
+        context() {}
+        virtual ~context() {}
 
-public:
-    void addVertex(const aten::vertex& vtx)
-    {
-        m_vertices.push_back(vtx);
-    }
+    public:
+        void addVertex(const aten::vertex& vtx)
+        {
+            m_vertices.push_back(vtx);
+        }
 
-    const aten::vertex& getVertex(int idx) const
-    {
-        return m_vertices[idx];
-    }
+        const aten::vertex& getVertex(int idx) const
+        {
+            return m_vertices[idx];
+        }
 
-    aten::vertex& getVertex(int idx)
-    {
-        return m_vertices[idx];
-    }
+        aten::vertex& getVertex(int idx)
+        {
+            return m_vertices[idx];
+        }
 
-    const std::vector<aten::vertex>& getVertices() const
-    {
-        return m_vertices;
-    }
+        const std::vector<aten::vertex>& getVertices() const
+        {
+            return m_vertices;
+        }
 
-    uint32_t getVertexNum() const
-    {
-        return (uint32_t)m_vertices.size();
-    }
+        uint32_t getVertexNum() const
+        {
+            return (uint32_t)m_vertices.size();
+        }
 
-    void copyVertices(std::vector<vertex>& dst) const
-    {
-        std::copy(
-            m_vertices.begin(),
-            m_vertices.end(),
-            std::back_inserter(dst));
-    }
+        void copyVertices(std::vector<vertex>& dst) const
+        {
+            std::copy(
+                m_vertices.begin(),
+                m_vertices.end(),
+                std::back_inserter(dst));
+        }
 
-    void build();
+        void build();
 
-    const aten::GeomVertexBuffer& getVB() const
-    {
-        return m_vb;
-    }
+        const aten::GeomVertexBuffer& getVB() const
+        {
+            return m_vb;
+        }
 
-    void release()
-    {
-        m_vertices.clear();
-        m_vb.clear();
-    }
+        void release()
+        {
+            m_vertices.clear();
+            m_vb.clear();
+        }
 
-    void addMaterial(AT_NAME::material* mtrl);
+        void addMaterial(AT_NAME::material* mtrl);
 
-    DataList<AT_NAME::material>& getMaterials()
-    {
-        return m_materials;
-    }
+        DataList<AT_NAME::material>& getMaterials()
+        {
+            return m_materials;
+        }
 
-    const DataList<AT_NAME::material>& getMaterials() const
-    {
-        return m_materials;
-    }
+        const DataList<AT_NAME::material>& getMaterials() const
+        {
+            return m_materials;
+        }
 
-    int getMaterialNum() const
-    {
-        return static_cast<int>(getMaterials().size());
-    }
+        int getMaterialNum() const
+        {
+            return static_cast<int>(getMaterials().size());
+        }
 
-    AT_NAME::material* getMaterial(int idx)
-    {
-        AT_ASSERT(0 <= idx && idx < getMaterialNum());
-        return m_materials[idx];
-    }
+        AT_NAME::material* getMaterial(int idx)
+        {
+            AT_ASSERT(0 <= idx && idx < getMaterialNum());
+            return m_materials[idx];
+        }
 
-    const AT_NAME::material* getMaterial(int idx) const
-    {
-        AT_ASSERT(0 <= idx && idx < getMaterialNum());
-        return m_materials[idx];
-    }
+        const AT_NAME::material* getMaterial(int idx) const
+        {
+            AT_ASSERT(0 <= idx && idx < getMaterialNum());
+            return m_materials[idx];
+        }
 
-    void deleteAllMaterialsAndClearList();
+        void deleteAllMaterialsAndClearList();
 
-    void copyMaterialParameters(std::vector<MaterialParameter>& dst) const;
+        void copyMaterialParameters(std::vector<MaterialParameter>& dst) const;
 
-    const AT_NAME::material* findMaterialByName(const char* name) const;
+        const AT_NAME::material* findMaterialByName(const char* name) const;
 
-    int findMaterialIdxByName(const char* name) const;
+        int findMaterialIdxByName(const char* name) const;
 
-private:
-    std::vector<aten::vertex> m_vertices;
+    private:
+        std::vector<aten::vertex> m_vertices;
 
-    aten::GeomVertexBuffer m_vb;
+        aten::GeomVertexBuffer m_vb;
 
-    DataList<AT_NAME::material> m_materials;
-};
+        DataList<AT_NAME::material> m_materials;
+    };
 }

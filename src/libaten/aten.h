09b96d5639b8694e708be8d108c2aa727a595641
nackdai <newvf91.brjtn579.dn@gmail.com>
2019-08-09 08:49:55 +0900

    Remove unused include

--- src/libaten/aten.h
+++ src/libaten/aten.h
@@ -85,7 +85,6 @@
 #include "accelerator/threaded_bvh.h"
 #include "accelerator/stackless_bvh.h"
 #include "accelerator/stackless_qbvh.h"
-#include "accelerator/CullingFrusta.h"
 
 #include "accelerator/GpuPayloadDefs.h"
 

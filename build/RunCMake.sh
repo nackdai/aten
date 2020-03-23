53a7fd43c340cf9fd3e7c6d0febb881591950637
nackdai <newvf91.brjtn579.dn@gmail.com>
2020-03-24 00:27:24 +0900

    Add option to CMakeLists.txt to remove them from shellscript

--- build/RunCMake.sh
+++ build/RunCMake.sh
@@ -11,9 +11,5 @@ cmake \
 -D CMAKE_BUILD_TYPE=${build_type} \
 -D CMAKE_CXX_COMPILER=/usr/bin/clang++-8 \
 -D CUDA_HOST_COMPILER=/usr/bin/clang-8 \
--D BUILD_UTILS=FALSE \
--D GLFW_BUILD_DOCS=FALSE \
--D GLFW_BUILD_EXAMPLES=FALSE \
--D GLFW_BUILD_TESTS=FALSE \
 -D CUDA_TARGET_COMPUTE_CAPABILITY=${compute_capability} \
 -L -G Ninja ..

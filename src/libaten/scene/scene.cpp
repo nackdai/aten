54a0df64fcd6c8c02f0b826925545f960b61b8e6
nackdai <newvf91.brjtn579.dn@gmail.com>
2021-03-29 14:28:24 +0900

    Remove unnecessary code

--- src/libaten/scene/scene.cpp
+++ src/libaten/scene/scene.cpp
@@ -42,7 +42,6 @@ namespace aten {
         real& selectPdf,
         LightSampleResult& sampleRes)
     {
-#if 1
         Light* light = nullptr;
 
         auto num = m_lights.size();
@@ -59,64 +58,6 @@ namespace aten {
         }
 
         return light;
-#else
-        // Resampled Importance Sampling.
-
-        std::vector<LightSampleResult> samples(m_lights.size());
-        std::vector<real> costs(m_lights.size());
-
-        real sumCost = 0;
-
-        for (int i = 0; i < m_lights.size(); i++) {
-            const auto light = m_lights[i];
-
-            samples[i] = light->sample(ctxt, org, nml, sampler);
-
-            const auto& lightsample = samples[i];
-
-            vec3 posLight = lightsample.pos;
-            vec3 nmlLight = lightsample.nml;
-            real pdfLight = lightsample.pdf;
-            vec3 dirToLight = normalize(lightsample.dir);
-
-            auto cosShadow = dot(nml, dirToLight);
-            auto dist2 = squared_length(lightsample.dir);
-            auto dist = aten::sqrt(dist2);
-
-            auto illum = color::luminance(lightsample.finalColor);
-
-            if (cosShadow > 0) {
-                if (light->isSingular() || light->isInfinite()) {
-                    costs[i] = illum * cosShadow / pdfLight;
-                }
-                else {
-                    costs[i] = illum * cosShadow / dist2 / pdfLight;
-                }
-                sumCost += costs[i];
-            }
-            else {
-                costs[i] = 0;
-            }
-        }
-
-        auto r = sampler->nextSample() * sumCost;
-
-        real sum = 0;
-
-        for (int i = 0; i < costs.size(); i++) {
-            const auto c = costs[i];
-            sum += c;
-
-            if (r <= sum && c > 0) {
-                auto light = m_lights[i];
-                sampleRes = samples[i];
-                selectPdf = c / sumCost;
-                return light;
-            }
-        }
-
-        return nullptr;
-#endif
     }
 
     Light* scene::sampleLight(

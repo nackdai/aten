87d35328007a6251650381ed3c8297b1deca25af
nackdai <newvf91.brjtn579.dn@gmail.com>
2022-01-16 23:14:39 +0900

    Standardize computing implicit light hit

--- src/libidaten/kernel/pt_standard_impl.cuh
+++ src/libidaten/kernel/pt_standard_impl.cuh
@@ -6,7 +6,7 @@
 #include "kernel/accelerator.cuh"
 
 namespace kernel {
-    AT_CUDA_INLINE __device__ bool hitShadowRay(
+    inline __device__ bool hitShadowRay(
         bool enableLod,
         const idaten::Context& ctxt,
         const idaten::ShadowRay& shadowRay)
@@ -45,5 +45,44 @@ namespace kernel {
             hitobj);
 
         return isHit;
+    }
+
+    inline __device__ void hitImplicitLight(
+        bool is_back_facing,
+        int bounce,
+        idaten::PathContrib& path_contrib,
+        idaten::PathAttribute& path_attrib,
+        idaten::PathThroughput& path_throughput,
+        const aten::ray& ray,
+        const aten::vec3& hit_pos,
+        const aten::vec3& hit_nml,
+        float hit_area,
+        const aten::MaterialParameter& mtrl)
+    {
+        if (!is_back_facing) {
+            float weight = 1.0f;
+
+            if (bounce > 0 && !path_attrib.isSingular) {
+                auto cosLight = dot(hit_nml, -ray.dir);
+                auto dist2 = aten::squared_length(hit_pos - ray.org);
+
+                if (cosLight >= 0) {
+                    auto pdfLight = 1 / hit_area;
+
+                    // Convert pdf area to sradian.
+                    // http://kagamin.net/hole/edubpt/edubpt_v100.pdf
+                    // p31 - p35
+                    pdfLight = pdfLight * dist2 / cosLight;
+
+                    weight = path_throughput.pdfb / (pdfLight + path_throughput.pdfb);
+                }
+            }
+
+            auto contrib = path_throughput.throughput * weight * static_cast<aten::vec3>(mtrl.baseColor);
+            path_contrib.contrib += make_float3(contrib.x, contrib.y, contrib.z);
+        }
+
+        // When ray hit the light, tracing will finish.
+        path_attrib.isTerminate = true;
     }
 }

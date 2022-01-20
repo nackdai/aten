a66655cb329b40b2b6193863da126bf27935715c
nackdai <newvf91.brjtn579.dn@gmail.com>
2022-01-20 23:17:23 +0900

    Enable building

--- src/idatentest/main.cpp
+++ src/idatentest/main.cpp
@@ -15,11 +15,10 @@
 #include "../common/scenedefs.h"
 
 #define ENABLE_ENVMAP
-//#define ENABLE_ATROUS
 
 static int WIDTH = 1280;
 static int HEIGHT = 720;
-static const char* TITLE = "idaten";
+static const char* TITLE = "ReSTIR";
 
 #ifdef ENABLE_OMP
 static uint32_t g_threadnum = 8;
@@ -33,12 +32,20 @@ static bool g_isCameraDirty = false;
 static aten::AcceleratedScene<aten::GPUBvh> g_scene;
 static aten::context g_ctxt;
 
-aten::ATrousDenoiser atrous;
-
-//static idaten::RayTracing g_tracer;
 static idaten::PathTracing g_tracer;
-
 static aten::visualizer* g_visualizer;
+
+static float g_avgcuda = 0.0f;
+static float g_avgupdate = 0.0f;
+
+static bool g_enableUpdate = false;
+
+static aten::TAA g_taa;
+
+static aten::FBO g_fbo;
+
+static aten::RasterizeRenderer g_rasterizer;
+static aten::RasterizeRenderer g_rasterizerAABB;
 
 static bool g_willShowGUI = true;
 static bool g_willTakeScreenShot = false;
@@ -46,11 +53,99 @@ static int g_cntScreenShot = 0;
 
 static int g_maxSamples = 1;
 static int g_maxBounce = 5;
+static auto g_curMode = idaten::ReSTIRPathTracing::Mode::ReSTIR;
+static auto g_curReSTIRMode = idaten::ReSTIRPathTracing::ReSTIRMode::ReSTIR;
+static auto g_curAOVMode = idaten::ReSTIRPathTracing::AOVMode::WireFrame;
+static auto g_enableProgressive = false;
+static bool g_showAABB = false;
 
 static float g_moveMultiply = 1.0f;
 
+static bool g_enableFrameStep = false;
+static bool g_frameStep = false;
+
+static bool g_pickPixel = false;
+
+void update()
+{
+    static float y = 0.0f;
+    static float d = -0.1f;
+
+    auto obj = getMovableObj();
+
+    if (obj) {
+        auto t = obj->getTrans();
+
+        if (y >= -0.1f) {
+            d = -0.01f;
+        }
+        else if (y <= -1.5f) {
+            d = 0.01f;
+
+        }
+
+        y += d;
+        t.y += d;
+
+        obj->setTrans(t);
+        obj->update();
+
+        auto accel = g_scene.getAccel();
+        accel->update(g_ctxt);
+
+        {
+            std::vector<aten::GeomParameter> shapeparams;
+            std::vector<aten::PrimitiveParamter> primparams;
+            std::vector<aten::LightParameter> lightparams;
+            std::vector<aten::MaterialParameter> mtrlparms;
+            std::vector<aten::vertex> vtxparams;
+
+            aten::DataCollector::collect(
+                g_ctxt,
+                g_scene,
+                shapeparams,
+                primparams,
+                lightparams,
+                mtrlparms,
+                vtxparams);
+
+            const auto& nodes = g_scene.getAccel()->getNodes();
+            const auto& mtxs = g_scene.getAccel()->getMatrices();
+
+            g_tracer.updateBVH(
+                shapeparams,
+                nodes,
+                mtxs);
+        }
+    }
+}
+
 void onRun(aten::window* window)
 {
+    if (g_enableFrameStep && !g_frameStep) {
+        return;
+    }
+
+    auto frame = g_tracer.frame();
+
+    g_frameStep = false;
+
+    float updateTime = 0.0f;
+
+    {
+        aten::timer timer;
+        timer.begin();
+
+        if (g_enableUpdate) {
+            update();
+        }
+
+        updateTime = timer.end();
+
+        g_avgupdate = g_avgupdate * (frame - 1) + updateTime;
+        g_avgupdate /= (float)frame;
+    }
+
     if (g_isCameraDirty) {
         g_camera.update();
 
@@ -64,9 +159,16 @@ void onRun(aten::window* window)
         g_visualizer->clear();
     }
 
-    atrous.getPositionMap()->clearAsGLTexture(aten::vec4(real(1)));
-    atrous.getNormalMap()->clearAsGLTexture(aten::vec4(real(0), real(0), real(0), real(-1)));
-    atrous.getAlbedoMap()->clearAsGLTexture(aten::vec4(real(1)));
+    aten::GLProfiler::begin();
+
+    g_rasterizer.drawSceneForGBuffer(
+        g_tracer.frame(),
+        g_ctxt,
+        &g_scene,
+        &g_camera,
+        &g_fbo);
+
+    auto rasterizerTime = aten::GLProfiler::end();
 
     aten::timer timer;
     timer.begin();
@@ -78,9 +180,23 @@ void onRun(aten::window* window)
 
     auto cudaelapsed = timer.end();
 
+    g_avgcuda = g_avgcuda * (frame - 1) + cudaelapsed;
+    g_avgcuda /= (float)frame;
+
+    aten::GLProfiler::begin();
+
     g_visualizer->render(false);
 
-    if (g_willTakeScreenShot) {
+    auto visualizerTime = aten::GLProfiler::end();
+
+    if (g_showAABB) {
+        g_rasterizerAABB.drawAABB(
+            &g_camera,
+            g_scene.getAccel());
+    }
+
+    if (g_willTakeScreenShot)
+    {
         static char buffer[1024];
         ::sprintf(buffer, "sc_%d.png\0", g_cntScreenShot);
 
@@ -94,37 +210,78 @@ void onRun(aten::window* window)
 
     if (g_willShowGUI)
     {
-        ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        ImGui::Text("cuda : %.3f ms", cudaelapsed);
+        ImGui::Text("[%d] %.3f ms/frame (%.1f FPS)", g_tracer.frame(), 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::Text("cuda : %.3f ms (avg : %.3f ms)", cudaelapsed, g_avgcuda);
+        ImGui::Text("update : %.3f ms (avg : %.3f ms)", updateTime, g_avgupdate);
         ImGui::Text("%.3f Mrays/sec", (WIDTH * HEIGHT * g_maxSamples) / real(1000 * 1000) * (real(1000) / cudaelapsed));
 
-        int prevSamples = g_maxSamples;
-        int prevDepth = g_maxBounce;
+        if (aten::GLProfiler::isEnabled()) {
+            ImGui::Text("GL : [rasterizer %.3f ms] [visualizer %.3f ms]", rasterizerTime, visualizerTime);
+        }
 
-        ImGui::SliderInt("Samples", &g_maxSamples, 1, 100);
-        ImGui::SliderInt("Bounce", &g_maxBounce, 1, 10);
+        auto is_input_samples = ImGui::SliderInt("Samples", &g_maxSamples, 1, 100);
+        auto is_input_bounce = ImGui::SliderInt("Bounce", &g_maxBounce, 1, 10);
 
-        if (prevSamples != g_maxSamples || prevDepth != g_maxBounce) {
+        if (is_input_samples || is_input_bounce) {
             g_tracer.reset();
         }
 
-        if constexpr (std::is_same<decltype(g_tracer), idaten::PathTracing>::value) {
-            static constexpr char* items[] = { "None", "Normal", "Albedo" };
+#if 0
+        static const char* items[] = { "ReSTIR", "PT", "AOV" };
 
-            auto aov_mode = g_tracer.getAovMode();
+        if (ImGui::Combo("mode", (int*)&g_curMode, items, AT_COUNTOF(items))) {
+            g_tracer.setMode(g_curMode);
+        }
 
-            if (ImGui::Combo("mode", reinterpret_cast<int*>(&aov_mode), items, std::size(items))) {
-                g_tracer.setAovMode(aov_mode);
+        if (g_curMode == idaten::ReSTIRPathTracing::Mode::ReSTIR) {
+            static const char* restir_items[] = { "ReSTIR", "SpatialReuse" };
+
+            if (ImGui::Combo("restir mode", (int*)&g_curReSTIRMode, restir_items, AT_COUNTOF(restir_items))) {
+                g_tracer.setReSTIRMode(g_curReSTIRMode);
             }
         }
 
-        bool enableProgressive = g_tracer.isProgressive();
+        if (g_curMode == idaten::ReSTIRPathTracing::Mode::AOVar) {
+            static const char* aovitems[] = { "Normal", "TexColor", "Depth", "Wire", "Barycentric", "Motion", "ObjId" };
 
-        if (ImGui::Checkbox("Progressive", &enableProgressive)) {
-            g_tracer.enableProgressive(enableProgressive);
+            if (ImGui::Combo("aov", (int*)&g_curAOVMode, aovitems, AT_COUNTOF(aovitems))) {
+                g_tracer.setAOVMode(g_curAOVMode);
+            }
+        }
+#endif
+
+        if (ImGui::Checkbox("Progressive", &g_enableProgressive)) {
+            g_tracer.setEnableProgressive(g_enableProgressive);
         }
 
+#if 0
+        bool enableTAA = g_taa.isEnableTAA();
+        bool canShowTAADiff = g_taa.canShowTAADiff();
+
+        if (ImGui::Checkbox("Enable TAA", &enableTAA)) {
+            g_taa.enableTAA(enableTAA);
+        }
+        if (ImGui::Checkbox("Show TAA Diff", &canShowTAADiff)) {
+            g_taa.showTAADiff(canShowTAADiff);
+        }
+#else
+        g_taa.enableTAA(false);
+#endif
+
+        ImGui::Checkbox("Show AABB", &g_showAABB);
+
+#if 0
+        bool canSSRTHitTest = g_tracer.canSSRTHitTest();
+        if (ImGui::Checkbox("Can SSRT Hit", &canSSRTHitTest)) {
+            g_tracer.setCanSSRTHitTest(canSSRTHitTest);
+        }
+#endif
+
         ImGui::SliderFloat("MoveMultiply", &g_moveMultiply, 1.0f, 100.0f);
+
+        auto cam = g_camera.param();
+        ImGui::Text("Pos %f/%f/%f", cam.origin.x, cam.origin.y, cam.origin.z);
+        ImGui::Text("At  %f/%f/%f", cam.center.x, cam.center.y, cam.center.z);
     }
 }
 
@@ -194,6 +351,28 @@ void onKey(bool press, aten::Key key)
             g_willTakeScreenShot = true;
             return;
         }
+        else if (key == aten::Key::Key_F3) {
+            g_enableFrameStep = !g_enableFrameStep;
+            return;
+        }
+        else if (key == aten::Key::Key_F4) {
+            g_enableUpdate = !g_enableUpdate;
+            return;
+        }
+        else if (key == aten::Key::Key_F5) {
+            aten::GLProfiler::trigger();
+            return;
+        }
+        else if (key == aten::Key::Key_SPACE) {
+            if (g_enableFrameStep) {
+                g_frameStep = true;
+                return;
+            }
+        }
+        else if (key == aten::Key::Key_CONTROL) {
+            g_pickPixel = true;
+            return;
+        }
     }
 
     auto offset = offset_base * g_moveMultiply;
@@ -235,7 +414,7 @@ void onKey(bool press, aten::Key key)
                 vfov,
                 WIDTH, HEIGHT);
         }
-            break;
+        break;
         default:
             break;
         }
@@ -260,6 +439,8 @@ int main()
         onMouseWheel,
         onKey);
 
+    aten::GLProfiler::start();
+
     g_visualizer = aten::visualizer::init(WIDTH, HEIGHT);
 
     aten::GammaCorrection gamma;
@@ -274,17 +455,32 @@ int main()
         "../shader/fullscreen_vs.glsl",
         "../shader/fullscreen_fs.glsl");
 
-    atrous.init(
-        g_ctxt,
+    g_taa.init(
         WIDTH, HEIGHT,
-        "../shader/fullscreen_vs.glsl", "../shader/atrous_fs.glsl",
-        "../shader/fullscreen_vs.glsl", "../shader/atrous_final_fs.glsl");
+        "../shader/fullscreen_vs.glsl", "../shader/taa_fs.glsl",
+        "../shader/fullscreen_vs.glsl", "../shader/taa_final_fs.glsl");
 
-#ifdef ENABLE_ATROUS
-    g_visualizer->addPostProc(&atrous);
-#endif
+    g_visualizer->addPostProc(&g_taa);
     g_visualizer->addPostProc(&gamma);
     //aten::visualizer::addPostProc(&blitter);
+
+    g_rasterizer.init(
+        WIDTH, HEIGHT,
+        "../shader/ssrt_vs.glsl",
+        "../shader/ssrt_gs.glsl",
+        "../shader/ssrt_fs.glsl");
+    g_rasterizerAABB.init(
+        WIDTH, HEIGHT,
+        "../shader/simple3d_vs.glsl",
+        "../shader/simple3d_fs.glsl");
+
+    g_fbo.asMulti(2);
+    g_fbo.init(
+        WIDTH, HEIGHT,
+        aten::PixelFormat::rgba32f,
+        true);
+
+    g_taa.setMotionDepthBufferHandle(g_fbo.getTexHandle(1));
 
     aten::vec3 pos, at;
     real vfov;
@@ -300,10 +496,6 @@ int main()
     Scene::makeScene(g_ctxt, &g_scene);
     g_scene.build(g_ctxt);
 
-    g_tracer.getCompaction().init(
-        WIDTH * HEIGHT,
-        1024);
-
 #ifdef ENABLE_ENVMAP
     auto envmap = aten::ImageLoader::load("../../asset/envmap/studio015.hdr", g_ctxt);
     auto bg = std::make_shared<aten::envmap>();
@@ -314,6 +506,10 @@ int main()
 #endif
 
     {
+        auto aabb = g_scene.getAccel()->getBoundingbox();
+        auto d = aabb.getDiagonalLenght();
+        g_tracer.setHitDistanceLimit(d * 0.25f);
+
         std::vector<aten::GeomParameter> shapeparams;
         std::vector<aten::PrimitiveParamter> primparams;
         std::vector<aten::LightParameter> lightparams;
@@ -331,7 +527,6 @@ int main()
 
         const auto& nodes = g_scene.getAccel()->getNodes();
         const auto& mtxs = g_scene.getAccel()->getMatrices();
-        //aten::bvh::dumpCollectedNodes(nodes, "nodes.txt");
 
         std::vector<idaten::TextureResource> tex;
         {
@@ -367,22 +562,21 @@ int main()
             primparams, 0,
             vtxparams, 0,
             mtxs,
+            tex,
 #ifdef ENABLE_ENVMAP
-            tex, idaten::EnvmapResource(envmap->id(), ibl->getAvgIlluminace(), real(4)));
+            idaten::EnvmapResource(envmap->id(), ibl->getAvgIlluminace(), real(1)));
 #else
-            tex, idaten::EnvmapResource());
-#endif
-
-#if 1
-        g_tracer.enableExportToGLTextures(
-            atrous.getPositionMap()->getGLTexHandle(),
-            atrous.getNormalMap()->getGLTexHandle(),
-            atrous.getAlbedoMap()->getGLTexHandle(),
-            aten::vec3(real(1)));
+            idaten::EnvmapResource());
 #endif
     }
 
     aten::window::run();
+
+    aten::GLProfiler::terminate();
+
+    g_rasterizer.release();
+    g_rasterizerAABB.release();
+    g_ctxt.release();
 
     aten::window::terminate();
 }

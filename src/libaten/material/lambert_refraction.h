fe8cc3c66ad58226543eb88ad2f1b51768810c58
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-02-13 23:48:34 +0900

    Remove unused APIs

--- src/libaten/material/lambert_refraction.h
+++ src/libaten/material/lambert_refraction.h
@@ -128,15 +128,6 @@ namespace AT_NAME
             return ret;
         }
 
-        virtual AT_DEVICE_MTRL_API real computeFresnel(
-            const aten::vec3& normal,
-            const aten::vec3& wi,
-            const aten::vec3& wo,
-            real outsideIor = 1) const override final
-        {
-            return computeFresnel(&m_param, normal, wi, wo, outsideIor);
-        }
-
         static AT_DEVICE_MTRL_API real computeFresnel(
             const aten::MaterialParameter* mtrl,
             const aten::vec3& normal,

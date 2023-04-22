ee1e5148eadff7fdef50b93c2898b07bf406f896
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-04-22 22:05:11 +0900

    fix

--- scripts/enter_docker_container.sh
+++ scripts/enter_docker_container.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # shellcheck disable=SC1090
 source "${SCRIPT_DIR}/docker_util"
 
-work_dir="$(realpath ${1})"
+work_dir="$(realpath "${1}")"
 
 aten_dev_image="$(docker images -q aten:latest 2>/dev/null)"
 
@@ -17,7 +17,6 @@ WORKSPACE="/work"
 kill_container "${CONTAINER_NAME}"
 
 launch_docker "${CONTAINER_NAME}" "${aten_dev_image}" \
-    "-w ${WORKSPACE} \
-    --mount type=bind,src=${work_dir},target=${WORKSPACE} \
-    --mount type=bind,src=${PWD}/.home,target=${HOME}"
-    "bash"
+  "-w ${WORKSPACE} \
+  --mount type=bind,src=${work_dir},target=${WORKSPACE} \
+  --mount type=bind,src=${PWD}/.home,target=${HOME}" "bash"

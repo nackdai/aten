698c5435f913a4579f104b215c9d8bd211091ee1
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-04-22 21:47:10 +0900

    Update files for development environment

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,19 +3,51 @@
 set -eu
 set -o pipefail
 
-# TODO
-# Usage:
-# build.sh <build_config> <compute_capability> <source_code_root_dir>
-# ex) ./scripts/build.sh Release 75 .
+usage() {
+  cat <<EOF 1>&2
+Usage: $0 [Options]
+Options:
+  -b <build_config>
+  -c <compute_capability>
+  -d <source_code_root_dir>
+ex) ./scripts/build.sh -b Release -c 75 -d .
+EOF
+  exit 1
+}
 
-build_type=${1:-Release}
+build_type="Release"
+compute_capability="75"
+work_dir="."
 
-compute_capability=${2}
+while getopts "b:c:d:" opt; do
+  case "${opt}" in
+    b)
+      build_type="${OPTARG}"
+      ;;
+    c)
+      compute_capability="${OPTARG}"
+      ;;
+    d)
+      work_dir="${OPTARG}"
+      ;;
+    \?)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
 
-echo "Build Type is [${build_type}]"
-echo "Compute Capability is [${compute_capability}]"
+work_dir="${1:-"."}"
+work_dir="$(realpath "${work_dir}")"
 
-work_dir=$(realpath "${3}")
+if [[ ! -d "${work_dir}" ]]; then
+  echo "${work_dir} doesn't exist"
+  exit 1
+fi
+
+echo "Build config: [${build_type}]"
+echo "Compute Capability: [${compute_capability}]"
+echo "Build dir: [${work_dir}]"
 
 aten_image="$(docker images -q aten:latest 2>/dev/null)"
 
@@ -27,22 +59,25 @@ cmake_cmd="cmake \
   -D CMAKE_CXX_COMPILER=/usr/bin/clang++-9 \
   -D CUDA_HOST_COMPILER=/usr/bin/clang-9 \
   -D CUDA_TARGET_COMPUTE_CAPABILITY=${compute_capability} \
-  -L -G Ninja .."
+  -G Ninja .."
 
-function finally() {
-  docker kill "${CONTAINER_NAME}" >/dev/null 2>&1 || true
-  docker container rm "${CONTAINER_NAME}" >/dev/null 2>&1 || true
-}
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+# shellcheck disable=SC1090
+source "${SCRIPT_DIR}/docker_util"
 
-trap finally EXIT ERR
+if [[ -z "${aten_image}" ]]; then
+  echo "No docker image aten_dev::latest"
+  exit 1
+else
+  kill_container "${CONTAINER_NAME}"
 
-if [[ -n "${aten_image}" ]]; then
-  docker run -it -d \
-    --name "${CONTAINER_NAME}" \
-    -v "${work_dir}":"${WORKSPACE}" \
-    -w "${WORKSPACE}" \
-    "${aten_image}" >/dev/null 2>&1
+  launch_docker "${WORKSPACE}" "${aten_image}" \
+    "-d \
+     -w ${WORKSPACE} \
+     --mount type=bind,src=${work_dir},target=${WORKSPACE} \
+     --mount type=bind,src=${PWD}/.home,target=${HOME} \
+     -e HOME=${HOME}"
 
-  docker exec "${CONTAINER_NAME}" bash -c "cd ${WORKSPACE} && mkdir -p build && cd build && ${cmake_cmd}"
+  docker exec "${CONTAINER_NAME}" bash -c "mkdir -p build && cd build && ${cmake_cmd}"
   docker exec "${CONTAINER_NAME}" bash -c "cd build && ninja -j 4"
 fi
---
+++ scripts/enter_docker_container.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+# shellcheck disable=SC1090
+source "${SCRIPT_DIR}/docker_util"
+
+work_dir="$(realpath ${1})"
+
+aten_dev_image="$(docker images -q aten:latest 2>/dev/null)"
+
+CONTAINER_NAME="aten_dev"
+WORKSPACE="/work"
+
+kill_container "${CONTAINER_NAME}"
+
+launch_docker "${CONTAINER_NAME}" "${aten_dev_image}" \
+    "-w ${WORKSPACE} \
+    --mount type=bind,src=${work_dir},target=${WORKSPACE} \
+    --mount type=bind,src=${PWD}/.home,target=${HOME}"
+    "bash"

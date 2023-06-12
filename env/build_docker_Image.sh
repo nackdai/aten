aa408768837c4a7b5bed1c8b989023bc21621e1f
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-06-12 23:13:56 +0900

    Update Dockerfile

--- env/build_docker_Image.sh
+++ env/build_docker_Image.sh
@@ -3,24 +3,42 @@
 set -eu
 set -o pipefail
 
-# TODO
-# Usage:
-# build_docker_iamge.sh <docker_file_stored_dir>
-# ex) ./env/build_docker_iamge.sh ./env
-
-function finally() {
-  popd
-  exit 0
+usage() {
+  cat <<EOF 1>&2
+Usage: $0 <OPTIONS>
+  -b <build_context>     : Docker build context.
+  -n <nvidia_cuda_image> : Base nvidia/cuda docker image
+  -t <nvidia_cudagl_tag> : Created cudagl image tag
+EOF
+  exit 1
 }
 
-SCRIPT_DIR=$(
-  cd "$(dirname "${0}")"
-  pwd
-)
+NVIDIA_CUDA_TAG="11.7.0-devel-ubuntu20.04"
 
-pushd "${SCRIPT_DIR}"
+build_context="./"
+nvidia_cuda="nvidia/cuda:${NVIDIA_CUDA_TAG}"
+nvidia_cudagl_tag="nvidia/cudagl:${NVIDIA_CUDA_TAG}"
 
-docker build -t aten ./aten
-docker build -t aten_dev --build-arg base_from=aten:latest -f ./dev/Dockerfile .
+while getopts "b:n:t:" opt; do
+  case "${opt}" in
+    b)
+      build_context="${OPTARG}"
+      ;;
+    n)
+      nvidia_cuda="${OPTARG}"
+      ;;
+    t)
+      nvidia_cudagl_tag="${OPTARG}"
+      ;;
+    \?)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
 
-trap finally EXIT
+build_context="$(realpath "${build_context}")"
+
+docker build -t "${nvidia_cudagl_tag}" --build-arg from="${nvidia_cuda}" -f "${build_context}/cudagl/Dockerfile" "${build_context}"
+docker build -t aten --build-arg from="${nvidia_cudagl_tag}" -f "${build_context}/aten/Dockerfile" "${build_context}"
+docker build -t aten_dev --build-arg from=aten:latest -f "${build_context}/dev/Dockerfile" "${build_context}"

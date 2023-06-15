37b0388a78282ccffe09d7eb7d3d94ecc5150263
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-06-16 00:20:39 +0900

    Fix docker image build script

--- env/build_docker_Image.sh
+++ env/build_docker_Image.sh
@@ -6,33 +6,28 @@ set -o pipefail
 usage() {
   cat <<EOF 1>&2
 Usage: $0 <OPTIONS>
-  -b <build_context>     : Docker build context.
-  -n <nvidia_cuda_image> : Base nvidia/cuda docker image
-  -t <nvidia_cudagl_tag> : Created cudagl image tag
-  -p <image_tag_prefix>  : Prefix for image tag
+  -b <build_context>         : Docker build context.
+  -n <nvidia_cuda_image_tag> : Base nvidia/cuda docker image
+  -p <image_tag_prefix>      : Prefix for image tag
 EOF
   exit 1
 }
 
-NVIDIA_CUDA_TAG="11.7.0-devel-ubuntu20.04"
-
 build_context="./"
-nvidia_cuda="nvidia/cuda:${NVIDIA_CUDA_TAG}"
-nvidia_cudagl_tag="nvidia/cudagl:${NVIDIA_CUDA_TAG}"
 image_tag_prefix=""
 
-while getopts "b:n:t:p:" opt; do
+NVIDIA_CUDA_TAG="11.7.0-devel-ubuntu20.04"
+
+while getopts "b:n:p:" opt; do
   case "${opt}" in
     b)
       build_context="${OPTARG}"
       ;;
     n)
-      nvidia_cuda="${OPTARG}"
+      NVIDIA_CUDA_TAG="${OPTARG}"
       ;;
-    t)
-      nvidia_cudagl_tag="${OPTARG}"
-      ;;
-    p):
+    p)
+      :
       image_tag_prefix="${OPTARG}"
       ;;
     \?)
@@ -44,6 +39,9 @@ shift $((OPTIND - 1))
 
 build_context="$(realpath "${build_context}")"
 
-docker build -t "${image_tag_prefix}${nvidia_cudagl_tag}" --build-arg from="${nvidia_cuda}" -f "${build_context}/cudagl/Dockerfile" "${build_context}"
-docker build -t "${image_tag_prefix}aten" --build-arg from="${nvidia_cudagl_tag}" -f "${build_context}/aten/Dockerfile" "${build_context}"
-docker build -t "${image_tag_prefix}aten_dev" --build-arg from=aten:latest -f "${build_context}/dev/Dockerfile" "${build_context}"
+nvidia_cuda="nvidia/cuda:${NVIDIA_CUDA_TAG}"
+nvidia_cudagl="nvidia/cudagl:${NVIDIA_CUDA_TAG}"
+
+docker build -t "${image_tag_prefix}${nvidia_cudagl}" --build-arg from="${nvidia_cuda}" -f "${build_context}/cudagl/Dockerfile" "${build_context}"
+docker build -t "${image_tag_prefix}aten" --build-arg from="${image_tag_prefix}${nvidia_cudagl}" -f "${build_context}/aten/Dockerfile" "${build_context}"
+docker build -t "${image_tag_prefix}aten_dev" --build-arg from="${image_tag_prefix}aten:latest" -f "${build_context}/dev/Dockerfile" "${build_context}"

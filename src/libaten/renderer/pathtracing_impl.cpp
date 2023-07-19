da5112348ce17c3c1ecd22d8c0b9ac3ba63e7f57
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-07-19 23:31:35 +0900

    Implement shade miss

--- src/libaten/renderer/pathtracing_impl.cpp
+++ src/libaten/renderer/pathtracing_impl.cpp
@@ -4,12 +4,11 @@
 namespace aten
 {
     void PathTracing::generate_path(
+        aten::ray& generated_ray,
         int32_t idx,
         int32_t ix, int32_t iy,
-        int32_t width, int32_t height,
         int32_t sample, uint32_t frame,
         aten::Path& paths,
-        aten::ray* rays,
         const aten::CameraParameter& camera,
         const uint32_t rnd)
     {
@@ -36,7 +35,7 @@ namespace aten
         AT_NAME::CameraSampleResult camsample;
         AT_NAME::PinholeCamera::sample(&camsample, &camera, s, t);
 
-        rays[idx] = camsample.r;
+        generated_ray = camsample.r;
 
         paths.throughput[idx].throughput = aten::vec3(1);
         paths.throughput[idx].pdfb = 1.0f;
@@ -49,8 +48,82 @@ namespace aten
         paths.contrib[idx].contrib = aten::vec3(0);
     }
 
-    void PathTracing::shader_miss_with_envmap()
+    void PathTracing::shader_miss(
+        int32_t idx,
+        int32_t bounce,
+        const aten::vec3& bg,
+        aten::Path& paths)
     {
+        if (!paths.attrib[idx].isTerminate && !paths.attrib[idx].isHit) {
+            if (bounce == 0) {
+                paths.attrib[idx].isKill = true;
+            }
 
+            auto contrib = paths.throughput[idx].throughput * bg;
+            paths.contrib[idx].contrib += aten::vec3(contrib.x, contrib.y, contrib.z);
+
+            paths.attrib[idx].isTerminate = true;
+        }
+    }
+
+    void PathTracing::shader_miss_with_envmap(
+        int32_t idx,
+        int32_t ix, int32_t iy,
+        int32_t width, int32_t height,
+        int32_t bounce,
+        int32_t envmap_idx,
+        float envmapAvgIllum, float envmapMultiplyer,
+        const aten::CameraParameter& camera,
+        aten::Path& paths,
+        const aten::ray& ray)
+    {
+        if (!paths.attrib[idx].isTerminate && !paths.attrib[idx].isHit) {
+            aten::vec3 dir = ray.dir;
+
+            if (bounce == 0) {
+                // Suppress jittering envrinment map.
+                // So, re-sample ray without random.
+
+                // TODO
+                // More efficient way...
+
+                float s = ix / (float)(width);
+                float t = iy / (float)(height);
+
+                AT_NAME::CameraSampleResult camsample;
+                AT_NAME::PinholeCamera::sample(&camsample, &camera, s, t);
+
+                dir = camsample.r.dir;
+            }
+
+            auto uv = AT_NAME::envmap::convertDirectionToUV(dir);
+
+#ifdef __CUDACC__
+            // envmapidx is index to array of textures in context.
+            // In GPU, sampleTexture requires texture id of CUDA. So, arguments is different.
+            const auto bg = tex2DLod<float4>(ctxt.textures[param.envmapidx], uv.x, uv.y);
+#else
+            const auto bg = AT_NAME::sampleTexture(envmap_idx, uv.x, uv.y, aten::vec4(1));
+#endif
+            auto emit = aten::vec3(bg.x, bg.y, bg.z);
+
+            float misW = 1.0f;
+            if (bounce == 0
+                || (bounce == 1 && paths.attrib[idx].isSingular))
+            {
+                paths.attrib[idx].isKill = true;
+            }
+            else {
+                auto pdfLight = AT_NAME::ImageBasedLight::samplePdf(emit, envmapAvgIllum);
+                misW = paths.throughput[idx].pdfb / (pdfLight + paths.throughput[idx].pdfb);
+
+                emit *= envmapMultiplyer;
+            }
+
+            auto contrib = paths.throughput[idx].throughput * misW * emit;
+            paths.contrib[idx].contrib += aten::vec3(contrib.x, contrib.y, contrib.z);
+
+            paths.attrib[idx].isTerminate = true;
+        }
     }
 }

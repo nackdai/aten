4cd9d902179adbf8694739aefe83663a36b01589
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-08-30 02:23:02 +0900

    Refactoring shell scripts

--- scripts/run_pre_commit.sh
+++ scripts/run_pre_commit.sh
@@ -5,19 +5,26 @@ set -o pipefail
 
 usage() {
   cat <<EOF 1>&2
-Usage: $0 <dir_to_run_pre_commit>
-ex) ./scripts/run_pre_commit.sh ./
+Usage: $0 <docker_image> <dir_to_run_pre_commit>
+  <docker_iamge>          : docker image to run build
+  <dir_to_run_pre_commit> : Directory to run pre_commit
+ex) $0 aten_dev:latest ./
 EOF
   exit 1
 }
 
-if (("${#}" != 1)); then
+if [ "${#}" -ne 2 ]; then
+  echo "Argumaents are not specified properly"
   usage
 fi
 
-work_dir="$(realpath "${1}")"
+docker_image="${1}"
+work_dir="$(realpath "${2}")"
 
-aten_dev_image="$(docker images -q aten_dev:latest 2>/dev/null)"
+if [[ ! -d "${work_dir}" ]]; then
+  echo "${work_dir} doesn't exist"
+  exit 1
+fi
 
 CONTAINER_NAME="aten_dev"
 WORKSPACE="/work"
@@ -27,16 +34,19 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # shellcheck disable=SC1090
 source "${SCRIPT_DIR}/docker_util"
 
+docker_image_id=$(get_image_id "${docker_image}")
+
 trap 'kill_container ${CONTAINER_NAME}' EXIT ERR
 
-if [[ -z "${aten_dev_image}" ]]; then
-  echo "No docker image aten_dev::latest"
+if [[ -z "${docker_image_id}" ]]; then
+  echo "No docker image ${docker_image}"
   exit 1
 else
   kill_container "${CONTAINER_NAME}"
 
-  # NOTE: https://stackoverflow.com/questions/62539288/where-does-pre-commit-install-environments
-  launch_docker "${CONTAINER_NAME}" "${aten_dev_image}" \
+  # NOTE:
+  # https://stackoverflow.com/questions/62539288/where-does-pre-commit-install-environments
+  launch_docker "${CONTAINER_NAME}" "${docker_image_id}" \
     "-d -w ${WORKSPACE} \
     --mount type=bind,src=${work_dir},target=${WORKSPACE} \
     --mount type=bind,src=${PWD}/.home,target=${HOME} \
--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -5,18 +5,21 @@ set -o pipefail
 
 usage() {
   cat <<EOF 1>&2
-Usage: $0 [work directory]
+Usage: $0 <docker_image> <work_directory>
+  <docker_iamge>   : docker image to run build
+  <work_directory> : Work directory. This is work directiory in docker container
+ex) $0 aten:latest ./
 EOF
   exit 1
 }
 
-if (("${#}" != 1)); then
-  usage
+docker_image="${1}"
+work_dir="$(realpath "${2}")"
+
+if [[ ! -d "${work_dir}" ]]; then
+  echo "${work_dir} doesn't exist"
+  exit 1
 fi
-
-work_dir="$(realpath "${1}")"
-
-aten_image="$(docker images -q aten:latest 2>/dev/null)"
 
 CONTAINER_NAME="aten"
 WORKSPACE="/work"
@@ -26,15 +29,17 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # shellcheck disable=SC1090
 source "${SCRIPT_DIR}/docker_util"
 
+docker_image_id=$(get_image_id "${docker_image}")
+
 trap 'kill_container ${CONTAINER_NAME}' EXIT ERR
 
-if [[ -z "${aten_image}" ]]; then
-  echo "No docker image aten_dev::latest"
+if [[ -z "${docker_image_id}" ]]; then
+  echo "No docker image ${docker_image}"
   exit 1
 else
   kill_container "${CONTAINER_NAME}"
 
-  launch_docker "${CONTAINER_NAME}" "${aten_image}" \
+  launch_docker "${CONTAINER_NAME}" "${docker_image_id}" \
     "-d \
     -w ${WORKSPACE} \
     --mount type=bind,src=${work_dir},target=${WORKSPACE}"

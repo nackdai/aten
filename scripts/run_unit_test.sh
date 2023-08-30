f1ad6453326bcaa1562ea4acfc62affa5cfa6fac
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-08-30 19:52:37 +0900

    Add shell script to run built executable

--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -7,42 +7,15 @@ usage() {
   cat <<EOF 1>&2
 Usage: $0 <docker_image> <work_directory>
   <docker_iamge>   : docker image to run build
-  <work_directory> : Work directory. This is work directiory in docker container
+  <work_directory> : Base directory to store executables
 ex) $0 aten:latest ./
 EOF
   exit 1
 }
 
 docker_image="${1}"
-work_dir="$(realpath "${2}")"
-
-if [[ ! -d "${work_dir}" ]]; then
-  echo "${work_dir} doesn't exist"
-  exit 1
-fi
-
-CONTAINER_NAME="aten"
-WORKSPACE="/work"
+work_dir="${2}"
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-# shellcheck disable=SC1090
-source "${SCRIPT_DIR}/docker_util"
-
-docker_image_id=$(get_image_id "${docker_image}")
-
-trap 'kill_container ${CONTAINER_NAME}' EXIT ERR
-
-if [[ -z "${docker_image_id}" ]]; then
-  echo "No docker image ${docker_image}"
-  exit 1
-else
-  kill_container "${CONTAINER_NAME}"
-
-  launch_docker "${CONTAINER_NAME}" "${docker_image_id}" \
-    "-d \
-    -w ${WORKSPACE} \
-    --mount type=bind,src=${work_dir},target=${WORKSPACE}"
-
-  docker exec "${CONTAINER_NAME}" bash -c "cd ${WORKSPACE}/build/aten_unittest && ./aten_unittest"
-fi
+bash -c "${SCRIPT_DIR}/run_executable.sh -b ${work_dir} -d ${docker_image} -e aten_unittest"

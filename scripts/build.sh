5ffe9c3d8e2a5506de24d36cdf92214576630d8b
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-09-16 00:18:21 +0900

    Add python and shell scripts to run clang-tidy

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,7 @@ Options:
   -b <build_config>       : Build configuration. Default is "Release"
   -c <compute_capability> : Compute capability for CUDA. No need to specify ".". If it's "7.5", it's specified as "75". Default is "75"
   -d <docker_iamge>       : docker image to run build. This option is necessary
+  -e                      : Only export compile_commands.json
 ex) $0 -b Release -c 75 -d aten:latest
 EOF
   exit 1
@@ -18,8 +19,9 @@ EOF
 build_type="Release"
 compute_capability="75"
 docker_image=""
+will_export_compile_commands_json=false
 
-while getopts "b:c:d:" opt; do
+while getopts "b:c:d:e" opt; do
   case "${opt}" in
     b)
       build_type="${OPTARG}"
@@ -29,6 +31,9 @@ while getopts "b:c:d:" opt; do
       ;;
     d)
       docker_image="${OPTARG}"
+      ;;
+    e)
+      will_export_compile_commands_json=true
       ;;
     \?)
       usage
@@ -49,8 +54,26 @@ cmake_cmd="cmake \
   -D CUDA_TARGET_COMPUTE_CAPABILITY=${compute_capability} \
   -G Ninja .."
 
+remove_docker_container_option=""
+if "${will_export_compile_commands_json}"; then
+  remove_docker_container_option="-r"
+fi
+
 # In first running, not remove docker container for second running.
-python3 ./scripts/docker_operator.py -i "${docker_image}" -c "mkdir -p build && cd build && ${cmake_cmd}"
+python3 ./scripts/docker_operator.py "${remove_docker_container_option}" -i "${docker_image}" -c "mkdir -p build && cd build && ${cmake_cmd}"
+
+COMPILE_COMMANDS_JSON="compile_commands.json"
+
+# Just exporting compile_commands.json.
+if "${will_export_compile_commands_json}"; then
+  if [ -e "${PWD}/build/${COMPILE_COMMANDS_JSON}" ]; then
+    mv "${PWD}/build/${COMPILE_COMMANDS_JSON}" "${PWD}/${COMPILE_COMMANDS_JSON}"
+    exit 0
+  else
+    echo "Not generated ${COMPILE_COMMANDS_JSON}"
+    exit 1
+  fi
+fi
 
 # No need to keep docker conainer after running command. So, specify remove option.
 python3 ./scripts/docker_operator.py -r -i "${docker_image}" -c "cd build && ninja -j 4"
---
+++ scripts/clang_tidy.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+usage() {
+  cat <<EOF 1>&2
+Usage: $0 [Options]
+Options:
+  -d <docker_iamge>    : docker image to run build.
+  -h <header_filter>   : Header filter. If nothing is specified, `aten/src` is specified.
+  -g <commit> <commit> : Specify git diff files as clang-tidy target files.
+ex) $0 -d aten_dev:latest -g -h ${PWD}/aten/src
+EOF
+  exit 1
+}
+
+docker_image=""
+header_filter="${PWD}/aten/src"
+declare -a git_diff_commits=()
+
+while getopts "d:h:g" opt; do
+  case "${opt}" in
+    d)
+      docker_image="${OPTARG}"
+      ;;
+    h)
+      header_filter="${OPTARG}"
+      ;;
+    g)
+      # OPTING causes unbound variable error in get opt. It is caused by "set -u".
+      # In order to suprress the error temporarily, call "set +u" here.
+      set +u
+
+      # NOTE:
+      # https://stackoverflow.com/questions/7529856/retrieving-multiple-arguments-for-a-single-option-using-getopts-in-bash
+      until [[ $(eval "echo \${$OPTIND}") =~ ^-.* ]] || [[ -z $(eval "echo \${$OPTIND}") ]]; do
+        git_diff_commits+=($(eval "echo \${$OPTIND}"))
+        OPTIND=$((OPTIND + 1))
+      done
+
+      set -u
+      ;;
+    \?)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+IGNORE_WORDS=("3rdparty" "imgui" "unittest")
+
+COMPILE_COMMANDS_JSON="compile_commands.json"
+
+# If compile_commands.json doesn't exist, generate.
+if [ ! -e "${COMPILE_COMMANDS_JSON}" ]; then
+  this_script_path="$(dirname -- "${BASH_SOURCE[0]}")"
+  if [ -e "${this_script_path}/build.sh" ]; then
+    "${this_script_path}"/build.sh -d "${docker_image}" -e
+  else
+    echo "No script to build ${COMPILE_COMMANDS_JSON}"
+    exit 1
+  fi
+fi
+
+declare -a target_files=()
+
+if (( ${#git_diff_commits[*]} > 0 )); then
+  if (( ${#git_diff_commits[*]} != 2 )); then
+    echo "2 commits need to be speified to -g option"
+    exit 1
+  fi
+  # Get diff files by added or modified.
+  target_files=($(git diff --diff-filter=AM --name-only "${git_diff_commits[0]}" "${git_diff_commits[1]}" | grep -E ".*\.(cpp|cxx|h|hpp)$"))
+fi
+
+# In order to pass as arguments with option, insert option "-t" at top.
+if (( ${#target_files[*]} > 0 )); then
+  target_files=("-t" "${target_files[@]:0}")
+fi
+
+python3 ./scripts/docker_operator.py -r -i "${docker_image}" -c "python3 ./scripts/run_clang_tidy.py -i ${IGNORE_WORDS[*]} -f ${header_filter} ${target_files[*]}"

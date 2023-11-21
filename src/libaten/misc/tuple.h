4de37808fdd1380e32d09e63d510b8193d93a9d0
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-11-22 00:25:19 +0900

    feat: Add forward_as_tuple

--- src/libaten/misc/tuple.h
+++ src/libaten/misc/tuple.h
@@ -9,6 +9,7 @@
 // NOTE
 // https://stackoverflow.com/questions/41660311/aliasing-a-std-template-function
 // https://stackoverflow.com/questions/2821223/how-would-one-call-stdforward-on-all-arguments-in-a-variadic-function
+// https://www.fluentcpp.com/2020/10/16/tie-make_tuple-forward_as_tuple-how-to-build-a-tuple-in-cpp/
 
 namespace aten {
 #ifdef __CUDACC__
@@ -29,6 +30,12 @@ namespace aten {
     constexpr __host__ __device__ decltype(auto) tie(Args&&... args) {
         return cuda::std::tie(std::forward<Args>(args)...);
     };
+
+    template <class... Args>
+    decltype(auto) forward_as_tuple(Args&&... args) {
+        return cuda::std::forward_as_tuple(std::forward<Args>(args)...);
+
+    }
 #else
     template <typename... _Types>
     using tuple = std::tuple<_Types...>;
@@ -47,5 +54,11 @@ namespace aten {
     constexpr decltype(auto) tie(Args&&... args) {
         return std::tie(std::forward<Args>(args)...);
     };
+
+    template <class... Args>
+    decltype(auto) forward_as_tuple(Args&&... args) {
+        return std::forward_as_tuple(std::forward<Args>(args)...);
+
+    }
 #endif
 }

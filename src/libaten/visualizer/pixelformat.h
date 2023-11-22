08300cf90a7bd20c1fa371e2bf74297e672f143a
nackdai <newvf91.brjtn579.dn@gmail.com>
2023-11-22 23:07:55 +0900

    feat: Move GetBytesPerPxiel to separate from OpenGL specific header file

--- src/libaten/visualizer/pixelformat.h
+++ src/libaten/visualizer/pixelformat.h
@@ -6,4 +6,12 @@ namespace aten {
         rgba32f,
         rgba16f,
     };
+
+    inline int32_t GetBytesPerPxiel(PixelFormat fmt)
+    {
+        constexpr int32_t bpp[] = {
+            4, 16, 8,
+        };
+        return bpp[static_cast<size_t>(fmt)];
+    }
 }

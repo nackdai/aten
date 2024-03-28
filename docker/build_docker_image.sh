9b43f55e60fa4da79dc1cf819f5f6936e9aa9283
nackdai <newvf91.brjtn579.dn@gmail.com>
2024-03-28 13:59:02 +0900

    chore: Fix file name typo

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -9,17 +9,19 @@ Usage: $0 <OPTIONS>
   -b <build_context>         : Docker build context.
   -n <nvidia_cuda_image_tag> : Base nvidia/cuda docker image
   -p <image_tag_prefix>      : Prefix for image tag
-ex) $0 -b ./env -p foo
+  -c                         : Build with cleaning cache.
+ex) $0 -b ./docker -p foo
 EOF
   exit 1
 }
 
 build_context="./"
 image_tag_prefix=""
+dcoker_build_opitons=()
 
 NVIDIA_CUDA_TAG="11.7.1-devel-ubuntu20.04"
 
-while getopts "b:n:p:" opt; do
+while getopts "b:n:p:c" opt; do
   case "${opt}" in
     b)
       build_context="${OPTARG}"
@@ -28,8 +30,10 @@ while getopts "b:n:p:" opt; do
       NVIDIA_CUDA_TAG="${OPTARG}"
       ;;
     p)
-      :
       image_tag_prefix="${OPTARG}"
+      ;;
+    c)
+      dcoker_build_opitons+=("--no-cache")
       ;;
     \?)
       usage
@@ -44,7 +48,7 @@ build_context="$(realpath "${build_context}")"
 # Need space between ":" and "-1"
 tail_image_tag_prefix="${image_tag_prefix: -1}"
 
-# If tail charachter of image_tag_prefix is "/", remove it.
+# If tail character of image_tag_prefix is "/", remove it.
 if [[ "${tail_image_tag_prefix}" == "/" ]]; then
   image_tag_prefix="${image_tag_prefix/%?/}"
 fi
@@ -57,6 +61,6 @@ docker pull "${nvidia_cuda}"
 export DOCKER_BUILDKIT=0
 export COMPOSE_DOCKER_CLI_BUILD=0
 
-docker build -t "${image_tag_prefix}/${nvidia_cudagl}" --build-arg from="${nvidia_cuda}" -f "${build_context}/cudagl/Dockerfile" "${build_context}"
-docker build -t "${image_tag_prefix}/aten" --build-arg from="${image_tag_prefix}/${nvidia_cudagl}" -f "${build_context}/aten/Dockerfile" "${build_context}"
-docker build -t "${image_tag_prefix}/aten_dev" --build-arg from="${image_tag_prefix}/aten:latest" -f "${build_context}/dev/Dockerfile" "${build_context}"
+docker build "${dcoker_build_opitons[@]}" -t "${image_tag_prefix}/${nvidia_cudagl}" --build-arg from="${nvidia_cuda}" -f "${build_context}/cudagl/Dockerfile" "${build_context}"
+docker build "${dcoker_build_opitons[@]}" -t "${image_tag_prefix}/aten" --build-arg from="${image_tag_prefix}/${nvidia_cudagl}" -f "${build_context}/aten/Dockerfile" "${build_context}"
+docker build "${dcoker_build_opitons[@]}" -t "${image_tag_prefix}/aten_dev" --build-arg from="${image_tag_prefix}/aten:latest" -f "${build_context}/dev/Dockerfile" "${build_context}"

91c0fd4319233f9f68c98f9b7c8515b268d8a0a3
nackdai <newvf91.brjtn579.dn@gmail.com>
2024-04-08 23:23:57 +0900

    refactor: Refactor lambert material

--- src/libaten/material/lambert.h
+++ src/libaten/material/lambert.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include "material/material.h"
-#include "texture/texture.h"
-#include "material/sample_texture.h"
 
 namespace aten {
     class Values;
@@ -28,35 +26,18 @@ namespace AT_NAME
         virtual ~lambert() {}
 
     public:
-        static AT_HOST_DEVICE_API real pdf(
+        static AT_DEVICE_API real pdf(
             const aten::vec3& normal,
             const aten::vec3& wo)
         {
-            const auto c = aten::abs(dot(normal, wo));
-            const auto ret = c / AT_MATH_PI;
-            return ret;
+            return ComputeProbabilityToSampleOutputVector(normal, wo);
         }
 
         static AT_DEVICE_API aten::vec3 sampleDirection(
             const aten::vec3& normal,
             real r1, real r2)
         {
-            auto n = normal;
-            auto t = aten::getOrthoVector(n);
-            auto b = cross(n, t);
-
-            // Importance sampling with cosine factor.
-            r1 = 2 * AT_MATH_PI * r1;
-            const real r2s = sqrt(r2);
-
-            const real x = aten::cos(r1) * r2s;
-            const real y = aten::sin(r1) * r2s;
-            const real z = aten::sqrt(real(1) - r2);
-
-            aten::vec3 dir = normalize((t * x + b * y + n * z));
-            AT_ASSERT(dot(normal, dir) >= 0);
-
-            return dir;
+            return SampleReflectDirection(normal, r1, r2);
         }
 
         static AT_DEVICE_API aten::vec3 sampleDirection(
@@ -66,13 +47,12 @@ namespace AT_NAME
             const auto r1 = sampler->nextSample();
             const auto r2 = sampler->nextSample();
 
-            return sampleDirection(normal, r1, r2);
+            return SampleReflectDirection(normal, r1, r2);
         }
 
         static AT_DEVICE_API aten::vec3 bsdf(const aten::MaterialParameter* param)
         {
-            aten::vec3 ret = aten::vec3(1.0f) / AT_MATH_PI;
-            return ret;
+            return ComputeBRDF();
         }
 
         static AT_DEVICE_API void sample(
@@ -96,6 +76,62 @@ namespace AT_NAME
             AT_EDIT_MATERIAL_PARAM_TEXTURE(editor, m_param, normalMap);
 
             return AT_EDIT_MATERIAL_PARAM(editor, m_param, baseColor);
+        }
+
+        /**
+         * @brief Compute probability to sample specified output vector.
+         * @param[in] n Surface normal.
+         * @param[in] wo Output vector.
+         * @return Probability to sample output vector.
+         */
+        static inline AT_DEVICE_API float ComputeProbabilityToSampleOutputVector(
+            const aten::vec3& n,
+            const aten::vec3& wo)
+        {
+            const auto c = aten::abs(dot(n, wo));
+            const auto ret = c / AT_MATH_PI;
+            return ret;
+        }
+
+        /**
+         * @brief Sample direction for reflection.
+         * @param[in] n Macrosurface normal.
+         * @param[in] r1 Rondam value by uniforma sampleing.
+         * @param[in] r2 Rondam value by uniforma sampleing.
+         * @return Output reflect vector.
+         */
+        static inline AT_DEVICE_API aten::vec3 SampleReflectDirection(
+            const aten::vec3& n,
+            float r1, float r2)
+        {
+            // Importance sampling with cosine factor.
+            const auto theta = aten::acos(aten::sqrt(1.0F - r1));
+            const auto phi = AT_MATH_PI_2 * r2;
+
+            const auto costheta = aten::cos(theta);
+            const auto sintheta = aten::sqrt(1 - costheta * costheta);
+
+            const auto cosphi = aten::cos(phi);
+            const auto sinphi = aten::sqrt(1 - cosphi * cosphi);
+
+            auto t = aten::getOrthoVector(n);
+            auto b = cross(n, t);
+
+            auto dir = t * sintheta * cosphi + b * sintheta * sinphi + n * costheta;
+            dir = normalize(dir);
+
+            AT_ASSERT(dot(n, dir) >= 0);
+
+            return dir;
+        }
+
+        /**
+         * @brief Compute BRDF.
+         * @return BRDF.
+         */
+        static AT_DEVICE_API aten::vec3 ComputeBRDF()
+        {
+            return aten::vec3(1.0F) / AT_MATH_PI;
         }
     };
 }

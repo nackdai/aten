1738a7831276be206e1e054674a9902c13550ec2
nackdai <newvf91.brjtn579.dn@gmail.com>
2024-04-18 22:34:05 +0900

    feat: Implement swap
    Due to CUDA 11 doesn't support std::swap

--- src/libaten/misc/misc.h
+++ src/libaten/misc/misc.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <utility>
 
 namespace aten
 {
@@ -10,4 +11,16 @@ namespace aten
     struct array_size<std::array<T, N>> {
         static constexpr size_t size = N;
     };
+
+    template <class T>
+    inline AT_DEVICE_API void swap(T& a, T& b)
+    {
+#ifdef __CUDACC__
+        const auto& t = a;
+        a = b;
+        b = t;
+#else
+        std::swap(a, b);
+#endif
+    }
 }

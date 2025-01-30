8757fb8ceef1465825c12379110fbace3eaf56ca
nackdai <newvf91.brjtn579.dn@gmail.com>
2025-01-31 00:36:35 +0900

    feat: Add Image class to load image file

--- src/libatenscene/ImageLoader.cpp
+++ src/libatenscene/ImageLoader.cpp
@@ -1,23 +1,17 @@
-#include <map>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 #include "ImageLoader.h"
 #include "utility.h"
 
 namespace aten {
-    static std::string g_base;
+    std::string ImageLoader::base_path;
 
     void ImageLoader::setBasePath(const std::string& base)
     {
-        g_base = removeTailPathSeparator(base);
+        base_path = removeTailPathSeparator(base);
     }
 
     std::shared_ptr<texture> ImageLoader::load(
         const std::string& path,
-        context& ctxt,
-        ImgFormat fmt/*= ImgFormat::Fmt8Bit*/)
+        context& ctxt)
     {
         std::string pathname;
         std::string extname;
@@ -29,119 +23,19 @@ namespace aten {
             extname,
             filename);
 
-        return load(filename, path, ctxt, fmt);
-    }
-
-    template <class TYPE>
-    static void read(
-        TYPE* src,
-        texture* tex,
-        int32_t width,
-        int32_t height,
-        int32_t channel,
-        float norm)
-    {
-        int32_t skipChannel = channel;
-
-#pragma omp parallel for
-        for (int32_t y = 0; y < height; y++) {
-            for (int32_t x = 0; x < width; x++) {
-                int32_t idx = y * width + x;
-                idx *= skipChannel;
-
-                switch (channel) {
-                case 4:
-                    (*tex)(x, y, 3) = src[idx + 3] * norm;
-                case 3:
-                    (*tex)(x, y, 2) = src[idx + 2] * norm;
-                case 2:
-                    (*tex)(x, y, 1) = src[idx + 1] * norm;
-                case 1:
-                    (*tex)(x, y, 0) = src[idx + 0] * norm;
-                    break;
-                }
-            }
-        }
+        return load(filename, path, ctxt);
     }
 
     std::shared_ptr<texture> ImageLoader::load(
         const std::string& tag,
         const std::string& path,
-        context& ctxt,
-        ImgFormat fmt/*= ImgFormat::Fmt8Bit*/)
+        context& ctxt)
     {
         std::string fullpath = path;
-        if (!g_base.empty()) {
-            fullpath = g_base + "/" + fullpath;
+        if (!base_path.empty()) {
+            fullpath = base_path + "/" + fullpath;
         }
 
-        std::string pathname;
-        std::string extname;
-        std::string filename;
-
-        getStringsFromPath(
-            path,
-            pathname,
-            extname,
-            filename);
-
-        std::string texname = filename + extname;
-
-        auto stored_tex = ctxt.GetTextureByName(tag);
-        if (stored_tex) {
-            AT_PRINTF("There is same tag texture. [%s]\n", tag.c_str());
-            return stored_tex;
-        }
-
-        std::shared_ptr<texture> tex;
-
-        float* dst = nullptr;
-        int32_t width = 0;
-        int32_t height = 0;
-        int32_t channels = 0;
-
-        if (stbi_is_hdr(fullpath.c_str())) {
-            auto src = stbi_loadf(fullpath.c_str(), &width, &height, &channels, 0);
-            if (src) {
-                tex = ctxt.CreateTexture(width, height, channels, texname.c_str());
-                float norm = float(1);
-                read<float>(src, tex.get(), width, height, channels, norm);
-
-                STBI_FREE(src);
-            }
-        }
-        else {
-            void* src = nullptr;
-
-            if (fmt == ImgFormat::Fmt8Bit) {
-                src = stbi_load(fullpath.c_str(), &width, &height, &channels, 0);
-            }
-            else {
-                src = stbi_load_16(fullpath.c_str(), &width, &height, &channels, 0);
-            }
-
-            if (src) {
-                tex = ctxt.CreateTexture(width, height, channels, texname.c_str());
-
-                if (fmt == ImgFormat::Fmt8Bit) {
-                    float norm = float(1) / float(255);
-                    read<stbi_uc>((stbi_uc*)src, tex.get(), width, height, channels, norm);
-                }
-                else {
-                    float norm = float(1) / float(65535);
-                    read<uint16_t>((uint16_t*)src, tex.get(), width, height, channels, norm);
-                }
-
-                STBI_FREE(src);
-            }
-        }
-
-        if (!tex) {
-            AT_ASSERT(false);
-            AT_PRINTF("Failed load texture. [%s]\n", fullpath.c_str());
-            return nullptr;
-        }
-
-        return tex;
+        return Image::Load(tag, fullpath, ctxt);
     }
 }

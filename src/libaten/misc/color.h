3b61991f652c1e3af90bdc39c120d123beeec503
nackdai <newvf91.brjtn579.dn@gmail.com>
2025-02-03 23:06:28 +0900

    fix: Fix wrong color conversions

--- src/libaten/misc/color.h
+++ src/libaten/misc/color.h
@@ -60,25 +60,45 @@ namespace AT_NAME {
 
         static inline AT_HOST_DEVICE_API float luminance(float r, float g, float b)
         {
-            float ret = float(0.2126) * r + float(0.7152) * g + float(0.0722) * b;
+#if 0
+            // NOTE:
+            // ITU-R BT.601.
+            float ret = 0.299F * r + 0.587F * g + 0.114F * b;
+#else
+            // Y in XYZ from RGB.
+            float ret = r + 4.59062F * g + 0.06007F * b;
+#endif
             return ret;
+        }
+
+        static AT_DEVICE_API aten::vec3 sRGBtoXYZ(const aten::vec3& srgb)
+        {
+            // NOTE:
+            // CUDA compiler doesn't allow dynamic static initialization during compile.
+            const aten::vec3 _sRGB2X = aten::vec3(0.412391F, 0.357585F, 0.180482F);
+            const aten::vec3 _sRGB2Y = aten::vec3(0.212639F, 0.71517F, 0.0721926F);
+            const aten::vec3 _sRGB2Z = aten::vec3(0.0193308F, 0.119195F, 0.950536F);
+
+            auto x = dot(_sRGB2X, srgb);
+            auto y = dot(_sRGB2Y, srgb);
+            auto z = dot(_sRGB2Z, srgb);
+
+            return aten::vec3(x, y, z);
         }
 
         static AT_DEVICE_API aten::vec3 RGBtoXYZ(const aten::vec3& rgb)
         {
             // NOTE:
             // CUDA compiler doesn't allow dynamic static initialization during compile.
-            const aten::vec3 _RGB2X = aten::vec3(float(0.412391), float(0.357584), float(0.180481));
-            const aten::vec3 _RGB2Y = aten::vec3(float(0.212639), float(0.715169), float(0.072192));
-            const aten::vec3 _RGB2Z = aten::vec3(float(0.019331), float(0.119195), float(0.950532));
+            const aten::vec3 _RGB2X = aten::vec3(2.7689F, 1.7517F, 1.1302F);
+            const aten::vec3 _RGB2Y = aten::vec3(1.0000F, 4.5907F, 0.0601F);
+            const aten::vec3 _RGB2Z = aten::vec3(0.0000F, 0.0565F, 5.6943F);
 
             auto x = dot(_RGB2X, rgb);
             auto y = dot(_RGB2Y, rgb);
             auto z = dot(_RGB2Z, rgb);
 
-            aten::vec3 xyz = aten::vec3(x, y, z);
-
-            return xyz;
+            return aten::vec3(x, y, z);
         }
     };
 

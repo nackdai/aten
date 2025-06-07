536ef8b36c80616a3e3d20e38397c57e1bab7aad
nackdai <newvf91.brjtn579.dn@gmail.com>
2025-06-07 18:24:53 +0900

    refactor: Fix typo and simplify code

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -12,14 +12,14 @@ Usage: $0 <OPTIONS>
   -p <image_tag_prefix>      : Prefix for image tag
   --clear_cache              : Build with clearing cache.
 
-ex) $0 -b ./docker -b ./docker/ -p "ghcr.io/aten" -c 11.7.1 -u 20.04
+ex) $0 -b ./docker -p "ghcr.io/aten" -c 11.7.1 -u 20.04
 EOF
   exit 1
 }
 
 build_context="./"
 image_tag_prefix=""
-dcoker_build_opitons=()
+docker_build_options=()
 
 CUDA_VERSION="11.7.1"
 UBUNTU_VERSION="20.04"
@@ -31,7 +31,7 @@ while getopts "b:p:c:u:-:" opt; do
     -)
       case "${OPTARG}" in
         clear_cache)
-          dcoker_build_opitons+=("--no-cache")
+          docker_build_options+=("--no-cache")
           ;;
       esac
       ;;
@@ -56,14 +56,8 @@ shift $((OPTIND - 1))
 
 build_context="$(realpath "${build_context}")"
 
-# NOTE:
-# Need space between ":" and "-1"
-tail_image_tag_prefix="${image_tag_prefix: -1}"
-
-# If tail character of image_tag_prefix is "/", remove it.
-if [[ "${tail_image_tag_prefix}" == "/" ]]; then
-  image_tag_prefix="${image_tag_prefix/%?/}"
-fi
+# Remove trailing "/" from image_tag_prefix if present.
+image_tag_prefix="${image_tag_prefix%/}"
 
 export DOCKER_BUILDKIT=0
 export COMPOSE_DOCKER_CLI_BUILD=0
@@ -76,5 +70,5 @@ popd
 
 base_cudagl_image_tag="${base_cudagl_image_tag}:${CUDA_VERSION}-devel-ubuntu${UBUNTU_VERSION}"
 
-docker build "${dcoker_build_opitons[@]}" -t "${image_tag_prefix}/aten" --build-arg from="${base_cudagl_image_tag}" -f "${build_context}/aten/Dockerfile" "${build_context}"
-docker build "${dcoker_build_opitons[@]}" -t "${image_tag_prefix}/aten_dev" --build-arg from="${image_tag_prefix}/aten:latest" -f "${build_context}/dev/Dockerfile" "${build_context}"
+docker build "${docker_build_options[@]}" -t "${image_tag_prefix}/aten" --build-arg from="${base_cudagl_image_tag}" -f "${build_context}/aten/Dockerfile" "${build_context}"
+docker build "${docker_build_options[@]}" -t "${image_tag_prefix}/aten_dev" --build-arg from="${image_tag_prefix}/aten:latest" -f "${build_context}/dev/Dockerfile" "${build_context}"

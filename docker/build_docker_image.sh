0db72e137c7d3a09e24a2e4a9de54ec0ccd383e8
nackdai <newvf91.brjtn579.dn@gmail.com>
2025-12-03 01:37:02 +0900

    feat: Check if nvndia cudagl docker image is avaialble in local

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -3,14 +3,20 @@
 set -eu
 set -o pipefail
 
+CUDA_VERSION="12.5.1"
+UBUNTU_VERSION="22.04"
+
+TAG="latest"
+
 usage() {
   cat <<EOF 1>&2
 Usage: $0 <OPTIONS>
-  -b <build_context>         : Docker build context.
-  -c <cuda_version>          : CUDA version. Default is "12.5.1"
-  -u <ubuntu_version>        : Ubuntu version. Default is "22.04"
-  -p <image_tag_prefix>      : Prefix for image tag
-  --clear_cache              : Build with clearing cache.
+  -b <build_context>      : Docker build context.
+  -c <cuda_version>       : CUDA version. Default is "${CUDA_VERSION}"
+  -u <ubuntu_version>     : Ubuntu version. Default is "${UBUNTU_VERSION}"
+  -p <image_tag_prefix>   : Prefix for image tag
+  -t <tag>                : Specify tag. Default is "${TAG}"
+  --no-cache              : Build with --no-cache option.
 
 ex) $0 -b ./docker -p "ghcr.io/nackdai/aten" -c 12.5.1 -u 22.04
 EOF
@@ -21,16 +27,13 @@ build_context="./"
 image_tag_prefix=""
 docker_build_options=()
 
-CUDA_VERSION="12.5.1"
-UBUNTU_VERSION="22.04"
-
 CUDAGL_BUILD_DIR="3rdparty/nvidia_container_image_cuda"
 
-while getopts "b:p:c:u:-:" opt; do
+while getopts "b:p:c:u:t:-:f" opt; do
   case "${opt}" in
     -)
       case "${OPTARG}" in
-        clear_cache)
+        no-cache)
           docker_build_options+=("--no-cache")
           ;;
       esac
@@ -47,6 +50,9 @@ while getopts "b:p:c:u:-:" opt; do
     u)
       UBUNTU_VERSION="${OPTARG}"
       ;;
+    t)
+      TAG="${OPTARG}"
+      ;;
     \?)
       usage
       ;;
@@ -62,13 +68,20 @@ image_tag_prefix="${image_tag_prefix%/}"
 export DOCKER_BUILDKIT=0
 export COMPOSE_DOCKER_CLI_BUILD=0
 
-base_cudagl_image_tag="${image_tag_prefix}/nvidia/cudagl"
+base_cudagl_image_prefix="${image_tag_prefix}/nvidia/cudagl"
+base_cudagl_image_tag="${base_cudagl_image_prefix}:${CUDA_VERSION}-devel-ubuntu${UBUNTU_VERSION}"
+
+if docker image inspect "${base_cudagl_image_tag}" >/dev/null 2>/dev/null; then
+   echo "${base_cudagl_image_tag} is already available"
+else
+   # Try to pull the docker image. If it's not available, building it happens.
+   # So, even if it's failed, to continue to the following sequence, need to suppress to raise an error.
+   docker pull "${base_cudagl_image_tag}" >/dev/null 2>/dev/null || true
+fi
 
 pushd "${CUDAGL_BUILD_DIR}"
-./build.sh -d --image-name "${base_cudagl_image_tag}" --cuda-version "${CUDA_VERSION}" --os ubuntu --os-version "${UBUNTU_VERSION}" --arch x86_64 --cudagl
+./build.sh -d --image-name "${base_cudagl_image_prefix}" --cuda-version "${CUDA_VERSION}" --os ubuntu --os-version "${UBUNTU_VERSION}" --arch x86_64 --cudagl
 popd
 
-base_cudagl_image_tag="${base_cudagl_image_tag}:${CUDA_VERSION}-devel-ubuntu${UBUNTU_VERSION}"
-
-docker build "${docker_build_options[@]}" -t "${image_tag_prefix}/aten" --build-arg from="${base_cudagl_image_tag}" -f "${build_context}/aten/Dockerfile" "${build_context}"
-docker build "${docker_build_options[@]}" -t "${image_tag_prefix}/aten_dev" --build-arg from="${image_tag_prefix}/aten:latest" -f "${build_context}/dev/Dockerfile" "${build_context}"
+docker build "${docker_build_options[@]}" -t "${image_tag_prefix}/aten:${TAG}" --build-arg from="${base_cudagl_image_tag}" -f "${build_context}/aten/Dockerfile" "${build_context}"
+docker build "${docker_build_options[@]}" -t "${image_tag_prefix}/aten_dev:${TAG}" --build-arg from="${image_tag_prefix}/aten:latest" -f "${build_context}/dev/Dockerfile" "${build_context}"
